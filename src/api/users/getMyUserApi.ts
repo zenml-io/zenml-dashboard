bab09d95c803b1478292d6a3c7e1659ca06cba61
talhainvenxion <tqureshi@snoonu.com>
2022-08-22 19:07:16 +0500

    mock getMyUserApi response in catch

--- src/api/users/getMyUserApi.ts
+++ src/api/users/getMyUserApi.ts
@@ -2,6 +2,7 @@ import { fetchApiWithAuthRequest } from '../fetchApi';
 import { endpoints } from '../endpoints';
 import { httpMethods } from '../constants';
 import { apiUrl } from '../apiUrl';
+import mockApi from '../mockApiData';
 
 const getMyUserApi = ({
   authenticationToken,
@@ -12,6 +13,13 @@ const getMyUserApi = ({
     url: apiUrl(endpoints.users.me),
     method: httpMethods.get,
     authenticationToken,
+  }).catch((res) => {
+    if (process.env.REACT_APP_MOCKAPI_RESPONSE) {
+      res = {
+        data: mockApi.myUserMockResponse,
+      };
+    }
+    return res;
   });
 
 export default getMyUserApi;
