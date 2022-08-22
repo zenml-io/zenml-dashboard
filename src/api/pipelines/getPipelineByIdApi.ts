5a58862250ea3bd54275f1174de6a7926d1077f8
talhainvenxion <tqureshi@snoonu.com>
2022-08-22 19:06:13 +0500

    mock getPipelineByIdApi response in catch

--- src/api/pipelines/getPipelineByIdApi.ts
+++ src/api/pipelines/getPipelineByIdApi.ts
@@ -2,6 +2,7 @@ import { fetchApiWithAuthRequest } from '../fetchApi';
 import { endpoints } from '../endpoints';
 import { httpMethods } from '../constants';
 import { apiUrl } from '../apiUrl';
+import mockApi from '../mockApiData';
 
 const getPipelineByIdApi = ({
   authenticationToken,
@@ -14,6 +15,13 @@ const getPipelineByIdApi = ({
     url: apiUrl(endpoints.pipelines.get(pipelineId)),
     method: httpMethods.get,
     authenticationToken,
+  }).catch((res) => {
+    if (process.env.REACT_APP_MOCKAPI_RESPONSE) {
+      res = {
+        data: mockApi.pipelineByIdMockResponse,
+      };
+    }
+    return res;
   });
 
 export default getPipelineByIdApi;
