87bc8441ffd85f595483780ecf8589a466d3964f
talhainvenxion <talha@invenxion.com>
2022-09-18 19:01:43 +0500

    STACKCOMPONENT added

--- src/redux/actionTypes/stackComponentPages.ts
+++ src/redux/actionTypes/stackComponentPages.ts
@@ -1,7 +1,7 @@
 import { actionTypes } from './constants';
 
 export const stackComponentPagesActionTypes = {
-  setCurrentWorkspace: actionTypes.STACK_PAGES_SET_CURRENT_WORKSPACE,
-  setSelectedRunIds: actionTypes.STACK_PAGES_SET_SELECTED_RUN_IDS,
-  setFetching: actionTypes.STACK_PAGES_SET_FETCHING,
+  setCurrentWorkspace: actionTypes.STACKCOMPONENT_PAGES_SET_CURRENT_WORKSPACE,
+  setSelectedRunIds: actionTypes.STACKCOMPONENT_PAGES_SET_SELECTED_RUN_IDS,
+  setFetching: actionTypes.STACKCOMPONENT_PAGES_SET_FETCHING,
 };
