737ce0e050f71cb3ede609574f0b1b3d8affce9f
talhainvenxion <talha@invenxion.com>
2022-09-30 20:51:01 +0500

    fixed

--- src/api/organizations/getInviteByCodeApi.ts
+++ src/api/organizations/getInviteByCodeApi.ts
@@ -2,7 +2,7 @@ import { fetchApiWithAuthRequest } from '../fetchApi';
 import { endpoints } from '../endpoints';
 import { httpMethods } from '../constants';
 import { apiUrl } from '../apiUrl';
-import mockApi from '../mockApiData';
+// import mockApi from '../mockApiData';
 
 const getInviteByCodeApi = ({
   username,
