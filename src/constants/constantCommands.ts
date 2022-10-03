951e88bd42cf9ccc6f9d6df8a95bd2053b507a1d
Hamza Tahir <htahir111@gmail.com>
2022-10-03 23:03:22 +0200

    Constants

--- src/constants/constantCommands.ts
+++ src/constants/constantCommands.ts
@@ -1,18 +1,18 @@
 export const constantCommandsToCreateStack = {
   title: 'Create Stack',
-  documentation: 'https://google.com',
+  documentation: 'https://docs.zenml.io/starter-guide/stacks',
   body: [
     {
-      text: 'you can set it  active',
+      text: 'Create a pipeline',
       isCode: false,
     },
     {
-      text: 'yarn start',
+      text: 'zenml pipeline run -c config.yaml run.py',
       isCode: true,
     },
     {
       text: 'yarn start',
-      isCode: true,
+      isCode: false,
     },
     {
       text: 'yarn start',
@@ -34,43 +34,39 @@ export const constantCommandsToCreateStack = {
 };
 
 export const constantCommandsToCreatePipeline = {
-  title: 'Create Pipeline',
-  documentation: 'https://google.com',
+  title: 'Pipeline Cheatsheet',
+  documentation: 'https://docs.zenml.io/starter-guide/pipelines',
   body: [
     {
-      text: 'you can set it  active',
+      text: 'Create a pipeline',
       isCode: false,
     },
     {
-      text: 'yarn start',
+      text: 'zenml pipeline run -c config.yaml run.py',
       isCode: true,
     },
     {
-      text: 'yarn start',
-      isCode: true,
-    },
-    {
-      text: 'yarn start',
-      isCode: true,
-    },
-    {
-      text: 'yarn start',
-      isCode: true,
-    },
-    {
-      text: 'yarn start',
-      isCode: true,
-    },
-    {
-      text: 'you can set it  active',
+      text: 'List Pipelines',
       isCode: false,
+    },
+    {
+      text: 'zenml pipeline list',
+      isCode: true,
+    },
+    {
+      text: 'List pipeline runs',
+      isCode: false,
+    },
+    {
+      text: 'zenml pipeline runs list',
+      isCode: true,
     },
   ],
 };
 
 export const constantCommandsToCreateComponent = {
   // title: 'Create Pipeline',
-  documentation: 'https://google.com',
+  documentation: 'https://docs.zenml.io/starter-guide/stacks/stacks-components',
   componentCommand: {
     type: 'alerter',
     body: [
