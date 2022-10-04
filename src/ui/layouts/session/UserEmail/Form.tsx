fd44a074c64f2fa3e6ec3f4db864bf8ab53fca95
danialraza92 <muhammad.danial.raza@gmail.com>
2022-10-04 22:48:07 +0500

    Hotfix Missing API calls

--- src/ui/layouts/session/UserEmail/Form.tsx
+++ src/ui/layouts/session/UserEmail/Form.tsx
@@ -1,17 +1,18 @@
 import React, { useState } from 'react';
 import styles from './index.module.scss';
 import { loggedInRoute, toasterTypes } from '../../../../constants';
-import { showToasterAction } from '../../../../redux/actions';
 import {
-  Box,
-  FormEmailField,
-  PrimaryButton,
-} from '../../../components';
+  pipelinesActions,
+  showToasterAction,
+  stackComponentsActions,
+  stacksActions,
+} from '../../../../redux/actions';
+import { Box, FormEmailField, PrimaryButton } from '../../../components';
 import { useDispatch, usePushRoute, useSelector } from '../../../hooks';
 import { getTranslateByScope } from '../../../../services';
 import { sessionSelectors } from '../../../../redux/selectors/session';
 import { userSelectors } from '../../../../redux/selectors';
-import axios from 'axios'
+import axios from 'axios';
 
 export const Form: React.FC = () => {
   const { push } = usePushRoute();
@@ -20,20 +21,24 @@ export const Form: React.FC = () => {
 
   const user = useSelector(userSelectors.myUser);
   const authToken = useSelector(sessionSelectors.authenticationToken);
-  const userId = user?.id ? user?.id : ''
+  const userId = user?.id ? user?.id : '';
 
-  const [email, setEmail] = useState('')
-  const [submitting, setSubmitting] = useState(false)
-  const [skipSubmitting, setSkipSubmitting] = useState(false)
+  const [email, setEmail] = useState('');
+  const [submitting, setSubmitting] = useState(false);
+  const [skipSubmitting, setSkipSubmitting] = useState(false);
 
   const submit = async () => {
     setSubmitting(true);
     try {
-      await axios.put(`${process.env.REACT_APP_BASE_API_URL}/users/${userId}/email-opt-in`, { email, email_opted_in: true }, {
-        headers: {
-          'Authorization': `bearer ${authToken}` 
-        }
-      })
+      await axios.put(
+        `${process.env.REACT_APP_BASE_API_URL}/users/${userId}/email-opt-in`,
+        { email, email_opted_in: true },
+        {
+          headers: {
+            Authorization: `bearer ${authToken}`,
+          },
+        },
+      );
       setSubmitting(false);
       dispatch(
         showToasterAction({
@@ -41,26 +46,33 @@ export const Form: React.FC = () => {
           type: toasterTypes.success,
         }),
       );
-        push(loggedInRoute);
-      } catch (err) {
-        setSubmitting(false);
-        dispatch(
+      dispatch(stackComponentsActions.getTypes());
+      dispatch(pipelinesActions.getMy());
+      dispatch(stacksActions.getMy({}));
+      push(loggedInRoute);
+    } catch (err) {
+      setSubmitting(false);
+      dispatch(
         showToasterAction({
           description: translate('form.toasts.failed.text'),
           type: toasterTypes.failure,
-          }),
-        );
-      }
-  }
+        }),
+      );
+    }
+  };
 
   const skip = async () => {
     setSkipSubmitting(true);
     try {
-      await axios.put(`${process.env.REACT_APP_BASE_API_URL}/users/${userId}/email-opt-in`, { email: '', email_opted_in: false }, {
-        headers: {
-          'Authorization': `bearer ${authToken}` 
-        }
-      })
+      await axios.put(
+        `${process.env.REACT_APP_BASE_API_URL}/users/${userId}/email-opt-in`,
+        { email: '', email_opted_in: false },
+        {
+          headers: {
+            Authorization: `bearer ${authToken}`,
+          },
+        },
+      );
       setSkipSubmitting(false);
       dispatch(
         showToasterAction({
@@ -68,77 +80,79 @@ export const Form: React.FC = () => {
           type: toasterTypes.success,
         }),
       );
-        push(loggedInRoute);
-      } catch (err) {
-        setSkipSubmitting(false);
-        dispatch(
+      dispatch(stackComponentsActions.getTypes());
+      dispatch(pipelinesActions.getMy());
+      dispatch(stacksActions.getMy({}));
+      push(loggedInRoute);
+    } catch (err) {
+      setSkipSubmitting(false);
+      dispatch(
         showToasterAction({
           description: translate('form.toasts.failed.text'),
           type: toasterTypes.failure,
-          }),
-        );
-      } 
-  }
+        }),
+      );
+    }
+  };
 
   return (
     <Box>
-      
       <Box marginTop="lg">
         <FormEmailField
           label={translate('form.email.label')}
-          labelColor='#ffffff'
+          labelColor="#ffffff"
           placeholder={translate('form.email.placeholder')}
           value={email}
           onChange={(val: string) => setEmail(val)}
         />
       </Box>
-      
+
       <PrimaryButton
-        marginTop='md'
+        marginTop="md"
         className={styles.signUpButton}
         loading={submitting}
-        onClick={submit} >
+        onClick={submit}
+      >
         {translate('form.button.text')}
       </PrimaryButton>
 
       <PrimaryButton
-        marginTop='md'
+        marginTop="md"
         className={styles.signUpButton}
         loading={skipSubmitting}
-        onClick={skip}>
+        onClick={skip}
+      >
         {translate('form.skip.text')}
       </PrimaryButton>
     </Box>
   );
 };
 
-
-    // await dispatch(SaveUserEmail({ userId, email: '', email_opted_in: false, authenticationToken: authToken ? authToken : '' }))    
-    // dispatch(SaveUserEmail({ userId, email, email_opted_in: true, authenticationToken: authToken ? authToken : '' }))    
-    // import { SaveUserEmailAction } from '../../../../redux/actions/users/saveUserEmailAction';
-    // dispatch(
-    //   SaveUserEmailAction({
-    //     userId,
-    //     email,
-    //     onFailure: () => {
-    //       setSubmitting(false);
-    //       dispatch(
-    //         showToasterAction({
-    //           description: translate('toasts.failed.text'),
-    //           type: toasterTypes.failure,
-    //         }),
-    //       );
-    //     },
-    //     onSuccess: () => {
-    //       setSubmitting(false);
-    //       dispatch(
-    //         showToasterAction({
-    //           description: translate('toasts.successful.text'),
-    //           type: toasterTypes.success,
-    //         }),
-    //       );
-    //       push(loggedOutRoute);
-    //     },
-    //   }),
-    // );
-   
+// await dispatch(SaveUserEmail({ userId, email: '', email_opted_in: false, authenticationToken: authToken ? authToken : '' }))
+// dispatch(SaveUserEmail({ userId, email, email_opted_in: true, authenticationToken: authToken ? authToken : '' }))
+// import { SaveUserEmailAction } from '../../../../redux/actions/users/saveUserEmailAction';
+// dispatch(
+//   SaveUserEmailAction({
+//     userId,
+//     email,
+//     onFailure: () => {
+//       setSubmitting(false);
+//       dispatch(
+//         showToasterAction({
+//           description: translate('toasts.failed.text'),
+//           type: toasterTypes.failure,
+//         }),
+//       );
+//     },
+//     onSuccess: () => {
+//       setSubmitting(false);
+//       dispatch(
+//         showToasterAction({
+//           description: translate('toasts.successful.text'),
+//           type: toasterTypes.success,
+//         }),
+//       );
+//       push(loggedOutRoute);
+//     },
+//   }),
+// );
--- src/ui/layouts/session/UserEmail/index.tsx
+++ src/ui/layouts/session/UserEmail/index.tsx
@@ -7,7 +7,7 @@ import {
   Box,
   EaseInBox,
   Image,
-  FullWidthSpinner
+  FullWidthSpinner,
 } from '../../../components';
 import { Form } from './Form';
 
@@ -17,37 +17,47 @@ import image from '../imageNew.png';
 import { getTranslateByScope } from '../../../../services';
 import { routePaths } from '../../../../routes/routePaths';
 import { sessionSelectors } from '../../../../redux/selectors/session';
-import { usePushRoute, useSelector } from '../../../hooks';
+import { useDispatch, usePushRoute, useSelector } from '../../../hooks';
 import { loggedInRoute } from '../../../../constants';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import {
+  pipelinesActions,
+  stackComponentsActions,
+  stacksActions,
+} from '../../../../redux/actions';
 
 const UserEmail: React.FC = () => {
   const translate = getTranslateByScope('ui.layouts.UserEmail');
   const { push } = usePushRoute();
+  const dispatch = useDispatch();
   const authToken = useSelector(sessionSelectors.authenticationToken);
 
-  const [pageLoading, setPageLoading] = useState(false)
+  const [pageLoading, setPageLoading] = useState(false);
 
   useEffect(() => {
-    setPageLoading(true)
+    setPageLoading(true);
     const getUser = async () => {
-       await axios.get(`${process.env.REACT_APP_BASE_API_URL}/current-user`, {
+      await axios
+        .get(`${process.env.REACT_APP_BASE_API_URL}/current-user`, {
           headers: {
-            'Authorization': `bearer ${authToken}` 
-          }
-          })
-          .then((res) => {
-            const data = res.data
-            if (data) {
-              if (data?.email_opted_in !== null) {
-                push(loggedInRoute)
-              }
+            Authorization: `bearer ${authToken}`,
+          },
+        })
+        .then((res) => {
+          const data = res.data;
+          if (data) {
+            if (data?.email_opted_in !== null) {
+              dispatch(stackComponentsActions.getTypes());
+              dispatch(pipelinesActions.getMy());
+              dispatch(stacksActions.getMy({}));
+              push(loggedInRoute);
             }
-            setPageLoading(false) 
-          })
-      }
-    getUser()
+          }
+          setPageLoading(false);
+        });
+    };
+    getUser();
 
     // eslint-disable-next-line
   }, [])
@@ -59,34 +69,36 @@ const UserEmail: React.FC = () => {
   return (
     <EaseInBox>
       <Container className={styles.signUp} fluid>
-            <Box style={{ overflowY: 'auto' }}>
-              <Box margin="lg" style={{ textAlign: 'center' }}>
-                <Link to={routePaths.signup}>
-                  <Image src={image} />
-                </Link>
+        <Box style={{ overflowY: 'auto' }}>
+          <Box margin="lg" style={{ textAlign: 'center' }}>
+            <Link to={routePaths.signup}>
+              <Image src={image} />
+            </Link>
+          </Box>
+          <FlexBox.Column
+            paddingHorizontal="sm"
+            justifyContent="center"
+            className={styles.pageContent}
+          >
+            <Box marginTop="xl" marginBottom="lg">
+              <Box paddingBottom="md" style={{ textAlign: 'center' }}>
+                <H2 bold color="white">
+                  {translate('title')}
+                </H2>
               </Box>
-              <FlexBox.Column
-                paddingHorizontal="sm"
-                justifyContent="center"
-                className={styles.pageContent}
-              >
-                <Box marginTop="xl" marginBottom='lg'>
-                  <Box paddingBottom="md" style={{ textAlign: 'center'}}>
-                    <H2 bold color='white' >{translate('title')}</H2>
-                  </Box>
-                  <Box className={styles.pageContentBox}>
-                    <Paragraph color="grey">{translate('subtitle1')}</Paragraph>
-                  </Box>
-                  <Box marginTop='md' className={styles.pageContentBox}>
-                    <Paragraph color="grey">{translate('subtitle2')}</Paragraph>
-                  </Box>
-                </Box>
-
-                <Box className={styles.formBox}>
-                  <Form />
-                </Box>         
-              </FlexBox.Column>
+              <Box className={styles.pageContentBox}>
+                <Paragraph color="grey">{translate('subtitle1')}</Paragraph>
+              </Box>
+              <Box marginTop="md" className={styles.pageContentBox}>
+                <Paragraph color="grey">{translate('subtitle2')}</Paragraph>
+              </Box>
             </Box>
+
+            <Box className={styles.formBox}>
+              <Form />
+            </Box>
+          </FlexBox.Column>
+        </Box>
       </Container>
     </EaseInBox>
   );
