d0ebac119b9fc5a5d608b247a4927394e52382aa
talhainvenxion <talha@invenxion.com>
2022-10-12 16:57:48 +0500

    added refresh button in DAG to call api again.

--- src/ui/components/lineage/index.tsx
+++ src/ui/components/lineage/index.tsx
@@ -13,6 +13,8 @@ import StepNode from './StepNode';
 
 import './index.css';
 import { Analysis, Data, Model, Schema, Service, Statistic } from './icons';
+import { useDispatch } from '../../hooks';
+import { runsActions } from '../../../redux/actions';
 
 interface Edge {
   id: string;
@@ -109,6 +111,7 @@ const getLayoutedElements = (
 const nodeTypes = { step: StepNode, artifact: ArtifactNode };
 
 export const LayoutFlow: React.FC<any> = (graph: any) => {
+  const dispatch = useDispatch();
   const {
     initialNodes: layoutedNodes,
     initialEdges: layoutedEdges,
@@ -151,6 +154,18 @@ export const LayoutFlow: React.FC<any> = (graph: any) => {
   return (
     <>
       <div className="controls">
+        <button
+          onClick={() => {
+            console.log('checkGraph', graph.graph.runId);
+            dispatch(
+              runsActions.graphForRun({
+                runId: graph.graph.runId,
+              }),
+            );
+          }}
+        >
+          Refresh
+        </button>
         <button onClick={() => onLayout('TB')}>Vertical Layout</button>
         <button onClick={() => onLayout('LR')}>Horizontal Layout</button>
         <div style={{ position: 'relative' }}>
