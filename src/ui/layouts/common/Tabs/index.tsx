448c2e93258641b0db1489f4cc00bd0ac0b4adaa
AhsanKhan7 <innocent.ahsan12345@gmail.com>
2022-12-12 15:08:06 +0500

    tab updates

--- src/ui/layouts/common/Tabs/index.tsx
+++ src/ui/layouts/common/Tabs/index.tsx
@@ -1,68 +1,20 @@
 import React from 'react';
-import { Link, matchPath, Redirect, Switch } from 'react-router-dom';
+import { Redirect, Switch } from 'react-router-dom';
 import { AppRoute } from '../../../../routes';
-import { joinClassNames } from '../../../../utils';
 import {
-  Box,
   FlexBox,
   If,
-  IfElse,
-  Paragraph,
-  Truncate,
 } from '../../../components';
-import { useLocationPath } from '../../../hooks';
-
-import styles from './index.module.scss';
 
 export const Tabs: React.FC<{ pages: TabPage[]; basePath: string }> = ({
   pages,
   basePath,
 }) => {
-  const locationPath = useLocationPath();
-
   return (
     <>
       <If condition={pages.length > 0}>
         {() => (
-          <>
-            <FlexBox
-              marginTop="md"
-              className={styles.navigation}
-            >
-              {pages.map((page, index) => {
-                const isActive = !!matchPath(locationPath, {
-                  path: page.path,
-                  exact: true,
-                });
-
-                return (
-                  <Box
-                    key={index}
-                    className={joinClassNames(
-                      styles.item,
-                      isActive ? styles.activeItem : '',
-                    )}
-                  >
-                    <Link className={styles.link} to={page.path}>
-                      <IfElse
-                        condition={isActive}
-                        renderWhenFalse={() => (
-                          <Truncate maxLines={1}>
-                            <Paragraph color="white">{page.text}</Paragraph>
-                          </Truncate>
-                        )}
-                        renderWhenTrue={() => (
-                          <Truncate maxLines={1}>
-                            <Paragraph color="white">{page.text}</Paragraph>
-                          </Truncate>
-                        )}
-                      />
-                    </Link>
-                  </Box>
-                );
-              })}
-            </FlexBox>
-            <FlexBox>
+            <FlexBox marginTop="xl">
               <Switch>
                 <Redirect exact from={basePath} to={pages[0].path} />
 
@@ -76,7 +28,6 @@ export const Tabs: React.FC<{ pages: TabPage[]; basePath: string }> = ({
                 ))}
               </Switch>
             </FlexBox>
-          </>
         )}
       </If>
     </>
