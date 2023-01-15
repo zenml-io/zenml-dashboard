20f3c303ee603be67d781770838040dbf52f3c98
talhainvenxion <talha@invenxion.com>
2023-01-16 02:35:04 +0500

    fixed component name if empty

--- src/ui/layouts/stackComponents/Stacks/List/index.tsx
+++ src/ui/layouts/stackComponents/Stacks/List/index.tsx
@@ -97,7 +97,7 @@ export const List: React.FC<Props> = ({ filter }: Props) => {
               }
             : {
                 text: `Nothing to see here, it seems like no ${camelCaseToParagraph(
-                  selectedProject,
+                  locationPath.split('/')[4],
                 )} has been configured yet.`,
               }
         }
