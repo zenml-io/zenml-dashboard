c70eb7c9d573b64b190eea1405bed75167cd9d27
talhainvenxion <talha@invenxion.com>
2023-01-24 14:44:51 +0500

    resizing in progress

--- src/ui/layouts/common/Table/index.tsx
+++ src/ui/layouts/common/Table/index.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import cn from 'classnames';
-
+import './styles.css';
 import styles from './index.module.scss';
 import {
   Box,
@@ -40,6 +40,7 @@ export interface HeaderCol {
 export interface TableProps {
   headerCols: HeaderCol[];
   tableRows: any[];
+  minCellWidth?: any;
   activeSorting?: any;
   paginated?: any;
   filters?: any[];
@@ -53,10 +54,18 @@ export interface TableProps {
   trOnClick?: (arg: any) => void;
 }
 
+const createHeaders = (headers: any[]) => {
+  return headers.map((item) => ({
+    text: item,
+    ref: useRef() as any,
+  }));
+};
+
 export const Table: React.FC<TableProps> = ({
   headerCols,
   tableRows,
   paginated,
+  minCellWidth = 120,
   activeSorting,
   filters,
   showHeader = true,
@@ -66,76 +75,69 @@ export const Table: React.FC<TableProps> = ({
   renderAfterRow,
   trOnClick,
 }) => {
-  // const [tableHeight, setTableHeight] = useState("auto");
-  // const [activeIndex, setActiveIndex] = useState(null);
-  // const tableElement = useRef(null);
-  // const createHeaders = (headers: any) => {
-  //   return headerCols?.map((item: any) => ({
-  //     text: item,
-  //     ref: tableElement
-  //   }));
-  // };
-  // const columns = createHeaders(headerCols?.length);
+  const [tableHeight, setTableHeight] = useState('auto');
+  const [activeIndex, setActiveIndex] = useState(null);
+  const tableElement = useRef(document.createElement('table'));
+  const columns = createHeaders(headerCols);
 
-  // const minCellWidth = 120
+  useEffect(() => {
+    console.log(tableElement.current.style.gridTemplateColumns, 'offsetHeight');
+    setTableHeight(tableElement.current.offsetHeight as any);
+  }, [tableElement.current]);
 
-  // useEffect(() => {
-  //   // @ts-ignore
-  //   // console.log(tableElement && tableElement.current.offsetHeight);
-  //   setTableHeight(tableElement?.current?.offsetHeight);
-  // }, []);
+  const mouseDown = (index: any) => {
+    setActiveIndex(index);
+  };
 
-  // const mouseDown = (index: any) => {
-  //   setActiveIndex(index);
-  // };
+  const mouseMove = useCallback(
+    (e) => {
+      // debugger;
+      const gridColumns = columns.map((col, i) => {
+        // debugger;
+        if (i === activeIndex) {
+          const width = e.clientX - col.ref.current.offsetLeft;
 
-  // const mouseMove = useCallback(
-  //   (e) => {
-  //     const gridColumns = columns.map((col: any, i: any) => {
-  //       if (i === activeIndex) {
-  //         const width = e.clientX - col.ref.current.offsetLeft;
+          if (width >= minCellWidth) {
+            return `${width}px`;
+          }
+        }
+        return `${col.ref.current.offsetWidth}px`;
+      });
 
-  //         if (width >= minCellWidth) {
-  //           return `${width}px`;
-  //         }
-  //       }
-  //       return `${col.ref.current.offsetWidth}px`;
-  //     });
+      tableElement.current.style.gridTemplateColumns = `${gridColumns.join(
+        ' ',
+      )}`;
+      console.log(tableElement.current.style.gridTemplateColumns, 'aasaaa');
+    },
+    [activeIndex, columns, minCellWidth],
+  );
 
-  //     // @ts-ignore
-  //     tableElement.current.style.gridTemplateColumns = `${gridColumns.join(
-  //       " "
-  //     )}`;
-  //   },
-  //   [activeIndex, columns, minCellWidth]
-  // );
+  const removeListeners = useCallback(() => {
+    window.removeEventListener('mousemove', mouseMove);
+    window.removeEventListener('mouseup', removeListeners);
+  }, [mouseMove]);
 
-  // const removeListeners = useCallback(() => {
-  //   window.removeEventListener("mousemove", mouseMove);
-  //   window.removeEventListener("mouseup", removeListeners);
-  // }, [mouseMove]);
+  const mouseUp = useCallback(() => {
+    setActiveIndex(null);
+    removeListeners();
+  }, [setActiveIndex, removeListeners]);
 
-  // const mouseUp = useCallback(() => {
-  //   setActiveIndex(null);
-  //   removeListeners();
-  // }, [setActiveIndex, removeListeners]);
+  useEffect(() => {
+    if (activeIndex !== null) {
+      window.addEventListener('mousemove', mouseMove);
+      window.addEventListener('mouseup', mouseUp);
+    }
 
-  // useEffect(() => {
-  //   if (activeIndex !== null) {
-  //     window.addEventListener("mousemove", mouseMove);
-  //     window.addEventListener("mouseup", mouseUp);
-  //   }
+    return () => {
+      removeListeners();
+    };
+  }, [activeIndex, mouseMove, mouseUp, removeListeners]);
 
-  //   return () => {
-  //     removeListeners();
-  //   };
-  // }, [activeIndex, mouseMove, mouseUp, removeListeners]);
-
-  // // Demo only
-  // const resetTableCells = () => {
-  //   // @ts-ignore
-  //   tableElement.current.style.gridTemplateColumns = "";
-  // };
+  // Demo only
+  const resetTableCells = () => {
+    debugger;
+    tableElement.current.style.gridTemplateColumns = '';
+  };
 
   const [showItems, setShowItems] = useState(false);
   const [fetchingMembers, setFetchingMembers] = useState(false);
@@ -265,6 +267,7 @@ export const Table: React.FC<TableProps> = ({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [locationPath.pathname.split('/')[4], isValidFilter, activeSorting]);
   let rowsToDisplay = tableRows;
+
   // function getFetchedState(state: any) {
   //   setFetchingMembers(state);
   //   // console.log(activeSorting, activeSortingDirection, 'aaaaaaa');
@@ -286,78 +289,167 @@ export const Table: React.FC<TableProps> = ({
   };
   console.log('pages11', itemPerPage, ITEMS_PER_PAGE);
   return (
-    <FlexBox.Column className={styles.tableWrapper} fullWidth>
+    <FlexBox.Column fullWidth>
       <IfElse
         condition={tableRows.length > 0 && !loading}
         renderWhenTrue={() => (
           <>
-            <table className={styles.table}>
-              <thead>
-                <tr className={showHeader ? styles.tableHeaderRow : ''}>
-                  {headerCols.map((headerCol: HeaderCol, index: number, i) => (
-                    <th
-                      className={styles.tableHeadingTh}
-                      style={{
-                        width: headerCol.width,
-                        color: '#424240',
-                        fontSize: '14px',
-                        fontWeight: 700,
-                      }}
-                      key={index}
-                    >
-                      <Box
-                        style={{ backgroundColor: '#f6f67' }}
-                        paddingVertical={showHeader ? 'sm' : null}
-                        paddingLeft="lg"
-                      >
-                        {headerCol.render && headerCol.render()}
-                      </Box>
-                    </th>
-                  ))}
-                </tr>
-              </thead>
-            </table>
-            {rowsToDisplay.map((headerRow: any, index: number) => (
-              <>
-                <table
-                  className={cn(
-                    styles.table,
-                    index + 1 === rowsToDisplay.length && styles.lastTable,
-                  )}
-                >
-                  <tbody key={index}>
-                    <tr
-                      onClick={() => trOnClick && trOnClick(headerRow)}
-                      className={cn(
-                        styles.tableRow,
-                        trOnClick && styles.clickableTableRow,
-                      )}
-                      style={{
-                        backgroundColor: index % 2 !== 0 ? '#F5F3F9' : 'white',
-                      }}
-                      key={index}
-                    >
-                      {headerCols.map((headerCol: HeaderCol, index: number) => (
-                        <td
-                          className={styles.tableTd}
-                          style={{ width: headerCol.width }}
-                          key={index}
+            <div>
+              <div>
+                <table ref={tableElement as any}>
+                  <thead>
+                    <tr style={{ backgroundColor: '#F5F3F9' }}>
+                      {console.log(columns, 'columns')}
+                      {columns.map(({ ref, text }, i) => (
+                        <th
+                          ref={ref}
+                          className={styles.tableHeadingTh}
+                          style={{
+                            // width: text.width,
+                            backgroundColor: '#F5F3F9',
+                            fontSize: '14px',
+                            fontWeight: 700,
+                          }}
+                          key={i}
                         >
-                          <Box paddingVertical="sm" paddingLeft="lg">
-                            <Truncate maxLines={1}>
-                              {headerCol.renderRow(headerRow)}
-                            </Truncate>
+                          <Box
+                            style={{ backgroundColor: '#F5F3F9' }}
+                            paddingVertical={showHeader ? 'sm' : null}
+                            paddingLeft="lg"
+                          >
+                            {text.render && text.render()}
                           </Box>
-                        </td>
+                          {console.log(tableHeight, 'tableHeight')}
+                          <div
+                            style={{ height: tableHeight }}
+                            onMouseDown={() => mouseDown(i)}
+                            className={`resize-handle ${
+                              activeIndex === i ? 'active' : 'idle'
+                            }`}
+                          />
+                        </th>
                       ))}
                     </tr>
-                  </tbody>
+                  </thead>
+
+                  {rowsToDisplay.map((headerRow: any, index: number) => (
+                    <tbody>
+                      <tr
+                        onClick={() => trOnClick && trOnClick(headerRow)}
+                        className={cn(
+                          styles.tableRow,
+                          trOnClick && styles.clickableTableRow,
+                        )}
+                        style={{
+                          backgroundColor:
+                            index % 2 !== 0 ? '#F5F3F9' : 'white',
+                        }}
+                        key={index}
+                      >
+                        {columns.map(({ ref, text }, i) => (
+                          <td
+                            className={styles.tableTd}
+                            style={{
+                              backgroundColor:
+                                index % 2 !== 0 ? '#F5F3F9' : 'white',
+                            }}
+                            key={i}
+                          >
+                            <Box paddingVertical="sm" paddingLeft="lg">
+                              <Truncate maxLines={1}>
+                                {text.renderRow(headerRow)}
+                              </Truncate>
+                            </Box>
+                          </td>
+                        ))}
+                      </tr>
+                      {/* <table className={styles.collapseTable}> */}
+                      <tbody>
+                        {renderAfterRow && renderAfterRow(headerRow)}
+                      </tbody>
+                      {/* </table> */}
+                    </tbody>
+                  ))}
                 </table>
-                <table className={styles.collapseTable}>
-                  <tbody>{renderAfterRow && renderAfterRow(headerRow)}</tbody>
-                </table>
-              </>
-            ))}
+              </div>
+              <button onClick={resetTableCells}>Reset</button>
+            </div>
+
+            {/* <table className={styles.table} ref={tableElement}>
+                <thead>
+                  <tr className={showHeader ? styles.tableHeaderRow : ''}>
+                    {headerCols.map((headerCol: HeaderCol, index: number) => (
+                      <th
+                        className={styles.tableHeadingTh}
+                        style={{
+                          width: headerCol.width,
+                          color: '#424240',
+                          fontSize: '14px',
+                          fontWeight: 700,
+                        }}
+                        key={index}
+                      >
+                        <Box
+                          style={{ backgroundColor: '#f6f67' }}
+                          paddingVertical={showHeader ? 'sm' : null}
+                          paddingLeft="lg"
+                        >
+                          {headerCol.render && headerColheaderCol.render && headerCol.render()}
+                        </Box>
+                        {console.log(tableHeight, 'tableHeight')}
+                        <div
+                          style={{ height: tableHeight }}
+                          onMouseDown={() => mouseDown(index)}
+                          className={`resize-handle ${
+                            activeIndex === index ? 'active' : 'idle'
+                          }`}
+                        />
+                      </th>
+                    ))}
+                  </tr>
+                </thead>
+                {rowsToDisplay.map((headerRow: any, index: number) => (
+                  <>
+                    <tbody key={index}>
+                      <tr
+                        onClick={() => trOnClick && trOnClick(headerRow)}
+                        className={cn(
+                          styles.tableRow,
+                          trOnClick && styles.clickableTableRow,
+                        )}
+                        style={{
+                          backgroundColor:
+                            index % 2 !== 0 ? '#F5F3F9' : 'white',
+                        }}
+                        key={index}
+                      >
+                        {headerCols.map(
+                          (headerCol: HeaderCol, index: number) => (
+                            <td
+                              className={styles.tableTd}
+                              style={{ width: headerCol.width }}
+                              key={index}
+                            >
+                              <Box paddingVertical="sm" paddingLeft="lg">
+                                <Truncate maxLines={1}>
+                                  {headerCol.renderRow(headerRow)}
+                                </Truncate>
+                              </Box>
+                            </td>
+                          ),
+                        )}
+                      </tr>
+                    </tbody>
+
+                    <table className={styles.collapseTable}>
+                      <tbody>
+                        {renderAfterRow && renderAfterRow(headerRow)}
+                      </tbody>
+                    </table>
+                  </>
+                ))}
+              </table> */}
+
             <If condition={pagination}>
               {() => (
                 <FlexBox
