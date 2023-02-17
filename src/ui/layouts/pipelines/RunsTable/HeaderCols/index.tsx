1c6e449367f0bc2840f64ccc578a1033707a1321
AhsanKhan7 <innocent.ahsan12345@gmail.com>
2023-02-17 20:10:09 +0500

    table and config UI updates

--- src/ui/layouts/pipelines/RunsTable/HeaderCols/index.tsx
+++ src/ui/layouts/pipelines/RunsTable/HeaderCols/index.tsx
@@ -169,13 +169,15 @@ export const useHeaderCols = ({
               activeSorting={activeSorting}
               activeSortingDirection={activeSortingDirection}
             >
-              <Paragraph
-                size="small"
-                color="black"
-                style={{ fontSize: '12px' }}
-              >
-                STATUS
-              </Paragraph>
+              <div style={{ margin: '0 auto 0 auto' }}>
+                <Paragraph
+                  size="small"
+                  color="black"
+                  style={{ fontSize: '12px', marginLeft: '-24px' }}
+                >
+                  STATUS
+                </Paragraph>
+              </div>
             </SortingHeader>
           ),
           width: '15%',
@@ -419,13 +421,15 @@ export const useHeaderCols = ({
               activeSorting={activeSorting}
               activeSortingDirection={activeSortingDirection}
             >
-              <Paragraph
-                size="small"
-                color="black"
-                style={{ fontSize: '12px' }}
-              >
-                STATUS
-              </Paragraph>
+              <div style={{ margin: '0 auto 0 auto' }}>
+                <Paragraph
+                  size="small"
+                  color="black"
+                  style={{ fontSize: '12px', marginLeft: '-24px' }}
+                >
+                  STATUS
+                </Paragraph>
+              </div>
             </SortingHeader>
           ),
           width: '15%',
--- src/ui/layouts/stackComponents/StackDetail/Configuration/index.tsx
+++ src/ui/layouts/stackComponents/StackDetail/Configuration/index.tsx
@@ -4,26 +4,32 @@ import {
   Box,
   EditField,
   Paragraph,
-  // PrimaryButton,
+  Row,
 } from '../../../../components';
 import styles from './index.module.scss';
 import { useService } from './useService';
+
 
 export const Configuration: React.FC<{ stackId: TId }> = ({ stackId }) => {
   const { stackComponent } = useService({
     stackId,
   });
-  console.log(stackComponent, 'stack222Component');
-  const getFormElement = (elementName: any, elementSchema: any) => {
-    // debugger;
 
+
+  const titleCase = (s: any) =>
+    s.replace(/^_*(.)|_+(.)/g, (s: any, c: string, d: string) =>
+      c ? c.toUpperCase() : ' ' + d.toUpperCase(),
+    );
+
+
+  const getFormElement = (elementName: any, elementSchema: any) => {
     if (typeof elementSchema === 'string') {
       return (
-        <Box style={{ width: '40%' }}>
+        <Box style={{ width: '90%' }}>
           <EditField
             disabled
             onChangeText={() => console.log('')}
-            label={elementName}
+            label={titleCase(elementName)}
             optional={false}
             value={elementSchema}
             placeholder=""
@@ -35,9 +41,9 @@ export const Configuration: React.FC<{ stackId: TId }> = ({ stackId }) => {
     }
     if (typeof elementSchema === 'object') {
       return (
-        <Box style={{ width: '40%' }}>
+        <Box style={{ width: '90%' }}>
           <Paragraph size="body" style={{ color: 'black' }}>
-            <label htmlFor={elementName}>{elementName}</label>
+            <label htmlFor={elementName}>{titleCase(elementName)}</label>
           </Paragraph>
           {Object.entries(elementSchema).map(([key, value]) => (
             <FlexBox.Row>
@@ -54,7 +60,7 @@ export const Configuration: React.FC<{ stackId: TId }> = ({ stackId }) => {
               <div style={{ width: '10%' }}></div>
               <EditField
                 disabled
-                marginRight={'md'}
+                // marginRight={'md'}
                 onChangeText={() => console.log('')}
                 label="Value"
                 // optional={true}
@@ -70,10 +76,13 @@ export const Configuration: React.FC<{ stackId: TId }> = ({ stackId }) => {
     }
     if (typeof elementSchema === 'boolean') {
       return (
-        <Box style={{ width: '40%' }} marginVertical="md">
+        <Box 
+        // marginVertical="md"
+        style={{ width: '90%' }}
+        >
           <Box>
             <FlexBox.Row justifyContent="space-between">
-              <Paragraph>{elementName}</Paragraph>
+              <Paragraph>{titleCase(elementName)}</Paragraph>
               <label className={styles.switch}>
                 <input type="checkbox" checked={elementSchema} />
                 <span className={`${styles.slider} ${styles.round}`}></span>
@@ -86,35 +95,42 @@ export const Configuration: React.FC<{ stackId: TId }> = ({ stackId }) => {
   };
 
   return (
-    <FlexBox.Column fullWidth marginTop="xl">
-      <Box style={{ width: '40%' }}>
-        <EditField
-          disabled
-          onChangeText={() => console.log('')}
-          label={'Flavor Name'}
-          optional={false}
-          value={stackComponent.flavor}
-          placeholder=""
-          hasError={false}
-          className={styles.field}
-        />
-      </Box>
-      <Box style={{ width: '40%' }} marginVertical="md">
-        <Box>
-          <FlexBox.Row justifyContent="space-between">
-            <Paragraph>Share Component with public</Paragraph>
-            <label className={styles.switch}>
-              <input type="checkbox" checked={stackComponent.isShared} />
-              <span className={`${styles.slider} ${styles.round}`}></span>
-            </label>
-          </FlexBox.Row>
-        </Box>
-      </Box>
-      {Object.keys(stackComponent?.configuration).map((key, ind) => (
-        <div key={key}>
-          {getFormElement(key, stackComponent?.configuration[key])}
-        </div>
-      ))}
+    <FlexBox justifyContent='space-between' marginLeft='md' marginTop="xl" fullWidth>
+      <Row>
+          <Box style={{ width: '46%' }}>
+            <EditField
+              disabled
+              onChangeText={() => console.log('')}
+              label={'Flavor Name'}
+              optional={false}
+              value={stackComponent.flavor}
+              placeholder=""
+              hasError={false}
+              className={styles.field}
+            />
+          </Box>
+          <Box style={{ width: '46%', marginLeft: '5%' }} marginTop='lg' >
+              <FlexBox.Row justifyContent="space-between">
+                <Paragraph>Share Component with public</Paragraph>
+                <label className={styles.switch}>
+                  <input type="checkbox" checked={stackComponent.isShared} />
+                  <span className={`${styles.slider} ${styles.round}`}></span>
+                </label>
+              </FlexBox.Row>
+          </Box>
+      
+      <Row>
+        {Object.keys(stackComponent?.configuration).map((key, ind) => (
+          <Row key={ind} style={{ width: '50%', marginLeft: 'auto' }}>
+            {getFormElement(key, stackComponent?.configuration[key])}
+          </Row>
+        ))}
+      </Row>
+      
+
+
+      </Row>
+
       {/* <FlexBox.Row>
         <Box style={{ width: '40%' }}>
           <Box>
@@ -195,6 +211,6 @@ export const Configuration: React.FC<{ stackId: TId }> = ({ stackId }) => {
       </FlexBox.Row> */}
 
       {/* <Box style={{ marginLeft: 'auto' }} marginRight='lg' ><PrimaryButton>Register Component</PrimaryButton></Box> */}
-    </FlexBox.Column>
+    </FlexBox>
   );
 };
