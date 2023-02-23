7d5467ecc0dd31074d1506052e9e28eaef7b8a66
talhainvenxion <talha@invenxion.com>
2023-02-23 22:46:15 +0500

    redern all components according to type.

--- src/ui/layouts/common/CustomStackBox/index.tsx
+++ src/ui/layouts/common/CustomStackBox/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Box, Paragraph } from '../../../components';
 
 import styles from './index.module.scss';
+import { titleCase } from '../../../../utils/camelCase';
 
 export const CustomStackBox: React.FC<{
   image: any;
@@ -9,29 +10,32 @@ export const CustomStackBox: React.FC<{
   stackDesc: string;
 }> = ({ image, stackName, stackDesc }) => {
   const [select, setSelect] = useState(false);
-
+  console.log(image, 'asdasd123123');
   return (
     <Box
       paddingHorizontal="sm2"
       paddingVertical="sm2"
       className={styles.stackBox}
     >
-    <input type='checkbox' className={styles.checkbox} checked={select} onClick={() => setSelect(!select)} />
+      <input
+        type="checkbox"
+        className={styles.checkbox}
+        checked={select}
+        onClick={() => setSelect(!select)}
+      />
       <Box className={styles.imageWrapper}>
         <Box className={styles.imageContainer}>
-            <img src={image} alt="by Zenml" />
+          <img src={image} alt="by Zenml" />
         </Box>
       </Box>
 
       <Box style={{ marginTop: '8px' }}>
-        <Paragraph className={styles.stackName}>
-          {stackName}
-        </Paragraph>
+        <Paragraph className={styles.stackName}>{stackName}</Paragraph>
       </Box>
 
       <Box marginTop="xs">
         <Paragraph className={styles.stackDesc}>
-          {stackDesc}
+          {titleCase(stackDesc)}
         </Paragraph>
       </Box>
     </Box>
--- src/ui/layouts/stacks/CreateStack/ListForAll/index.tsx
+++ src/ui/layouts/stacks/CreateStack/ListForAll/index.tsx
@@ -1,155 +1,99 @@
 import React from 'react';
-
-// import { CollapseTable } from '../../../common/CollapseTable';
-// import { useSelector } from '../../../../hooks';
-// import { useHistory } from 'react-router-dom';
-// import { routePaths } from '../../../../../routes/routePaths';
-
-// import { useService } from './useService';
-
-// import { camelCaseToParagraph } from '../../../../../utils';
-// import { DEFAULT_WORKSPACE_NAME } from '../../../../../constants';
-// import {
-//   workspaceSelectors,
-//   // stackComponentSelectors,
-//   flavorSelectors,
-// } from '../../../../../redux/selectors';
-import {
-  Box,
-  FlexBox,
-  // Paragraph,
-  // SearchInputField,
-  // Row,
-  H2,
-  FormTextField,
-  H3,
-} from '../../../../components';
+import { Box, FlexBox, H2, FormTextField } from '../../../../components';
 import { ToggleField } from '../../../common/FormElement';
-// import { PaginationWithPageSize } from '../../../common/PaginationWithPageSize';
-import { StackBox } from '../../../common/StackBox';
-import { CustomStackBox } from '../../../common/CustomStackBox';
-import logo from '../../../../assets/logo.svg'
-// import { callActionForFlavorsForPagination } from '../useService';
-// import { SidePopup } from '../../../common/SidePopup';
 
-interface Props {
-  // type: string;
-  // filter: any;
-  // pagination?: boolean;
-  // id?: string;
-  // isExpended?: boolean;
-}
+import { stackComponentSelectors } from '../../../../../redux/selectors';
+import { useSelector } from 'react-redux';
+import { GetList } from './GetList';
+import { GetFlavorsListForLogo } from '../../../stackComponents/Stacks/List/GetFlavorsListForLogo';
+
+interface Props {}
 
 export const ListForAll: React.FC<Props> = () => {
-  // const { dispatchFlavorsData } = callActionForFlavorsForPagination();
-  // const selectedWorkspace = useSelector(workspaceSelectors.selectedWorkspace);
-  // const flavorsPaginated = useSelector(flavorSelectors.myFlavorsPaginated);
-  // const [text, setText] = useState('');
-  // const [selectedFlavor, setSelectedFlavor] = useState() as any;
-  // const [selectedComponentId, setSelectedComponentId] = useState('');
-  // const [showModal, setShowModal] = useState(false);
-  // const history = useHistory();
-  // const { fetching, allFlavors } = useService();
+  const stackComponentsTypes: any[] = useSelector(
+    stackComponentSelectors.stackComponentTypes,
+  );
+  const { flavourList } = GetFlavorsListForLogo();
 
-  // function handleValueFieldChangeOnSearch(value: string) {
-  //   dispatchFlavorsData(1, flavorsPaginated.size, type, value);
-  // }
-  // const onSelectFlavor = (flavor: any) => {
-  //   setSelectedFlavor(flavor);
-  //   setShowModal(true);
-  // };
-  // const handleSelectedFlavor = (selectedFlavor: any) => {
-  //   setShowModal(false);
-  //   history.push(
-  //     routePaths.stackComponents.configureComponent(
-  //       type,
-  //       selectedWorkspace,
-  //       selectedFlavor?.id,
-  //     ),
-  //   );
-  // };
-
-  const helperTextStyle = {
-    fontSize: '16px', 
-    color: '#A8A8A8', 
-    marginLeft: '10px',
-    marginTop: '-3px'
-  };
-  
   return (
     <Box style={{ width: '100%' }}>
       <Box>
-        <H2 style={{ fontWeight: 'bolder' }} >Register a Stack</H2>
+        <H2 style={{ fontWeight: 'bolder' }}>Register a Stack</H2>
       </Box>
-    
-      <Box marginTop='lg'>
+
+      <Box marginTop="lg">
         <FlexBox.Row>
           <Box style={{ width: '30%' }}>
             <FormTextField
-                onChange={(e: any) => {}}
-                placeholder="Stack Name"
-                label={'Enter Stack Name'}
-                value=""
-              />
+              onChange={(e: any) => {}}
+              placeholder="Stack Name"
+              label={'Enter Stack Name'}
+              value=""
+            />
           </Box>
-          <Box marginLeft='xxxl' marginTop='md' style={{ width: '30%' }}>
-              <ToggleField
-                label={'Share Stack with public'}
-                onHandleChange={(key: any, value: any) => {}}
-              />
+          <Box marginLeft="xxxl" marginTop="md" style={{ width: '30%' }}>
+            <ToggleField
+              label={'Share Stack with public'}
+              onHandleChange={(key: any, value: any) => {}}
+            />
           </Box>
         </FlexBox.Row>
       </Box>
-    
-    <Box marginTop='md' style={{ overflowX: 'auto' }}>
-      <FlexBox.Row alignItems='center'>
-        <H3 style={{ fontWeight: 'bold' }}>Orchestrator</H3><span style={helperTextStyle}>&#40;8 Components&#41;</span>
-      </FlexBox.Row>
-      <FlexBox.Row>
-        <Box style={{ width: '171px' }} >
-          <StackBox stackName='Create' stackDesc='Create a stack' />
-        </Box>
-        {Array(8).fill(null)?.map(() => 
-          <Box marginLeft='md'>
-            <CustomStackBox image={logo} stackName='Sample' stackDesc='example text' />
-          </Box>
-        )}
-      </FlexBox.Row>
-    </Box>
+      <FlexBox.Column>
+        {stackComponentsTypes?.map((item) => {
+          return (
+            <Box marginTop="lg" style={{ overflowX: 'auto' }}>
+              <GetList type={item} flavourList={flavourList}></GetList>
+            </Box>
+          );
+        })}
+      </FlexBox.Column>
 
-    <Box marginTop='lg' style={{ overflowX: 'auto' }}>
-      <FlexBox.Row alignItems='center'>
-        <H3 style={{ fontWeight: 'bold' }}>Artifact Store</H3><span style={helperTextStyle}>&#40;5 Components&#41;</span>
-      </FlexBox.Row>
-      <FlexBox.Row>
-        <Box style={{ width: '171px' }} >
-          <StackBox stackName='Create' stackDesc='Create a stack' />
-        </Box>
-        {Array(5).fill(null)?.map(() => 
-          <Box marginLeft='md'>
-            <CustomStackBox image={logo} stackName='Sample' stackDesc='example text' />
+      {/* <Box marginTop="lg" style={{ overflowX: 'auto' }}>
+        <FlexBox.Row alignItems="center">
+          <H3 style={{ fontWeight: 'bold' }}>Artifact Store</H3>
+          <span style={helperTextStyle}>&#40;5 Components&#41;</span>
+        </FlexBox.Row>
+        <FlexBox.Row>
+          <Box style={{ width: '171px' }}>
+            <StackBox stackName="Create" stackDesc="Create a stack" />
           </Box>
-        )}
-      </FlexBox.Row>
-    </Box>
+          {Array(5)
+            .fill(null)
+            ?.map(() => (
+              <Box marginLeft="md">
+                <CustomStackBox
+                  image={logo}
+                  stackName="Sample"
+                  stackDesc="example text"
+                />
+              </Box>
+            ))}
+        </FlexBox.Row>
+      </Box> */}
 
-    <Box marginTop='lg' style={{ overflowX: 'auto' }}>
-      <FlexBox.Row alignItems='center'>
-        <H3 style={{ fontWeight: 'bold' }}>Secret Manager</H3><span style={helperTextStyle}>&#40;7 Components&#41;</span>
-      </FlexBox.Row>
-      <FlexBox.Row>
-        <Box style={{ width: '171px' }} >
-          <StackBox stackName='Create' stackDesc='Create a stack' />
-        </Box>
-        {Array(7).fill(null)?.map(() => 
-          <Box marginLeft='md'>
-            <CustomStackBox image={logo} stackName='Sample' stackDesc='example text' />
+      {/* <Box marginTop="lg" style={{ overflowX: 'auto' }}>
+        <FlexBox.Row alignItems="center">
+          <H3 style={{ fontWeight: 'bold' }}>Secret Manager</H3>
+          <span style={helperTextStyle}>&#40;7 Components&#41;</span>
+        </FlexBox.Row>
+        <FlexBox.Row>
+          <Box style={{ width: '171px' }}>
+            <StackBox stackName="Create" stackDesc="Create a stack" />
           </Box>
-        )}
-      </FlexBox.Row>
+          {Array(7)
+            .fill(null)
+            ?.map(() => (
+              <Box marginLeft="md">
+                <CustomStackBox
+                  image={logo}
+                  stackName="Sample"
+                  stackDesc="example text"
+                />
+              </Box>
+            ))}
+        </FlexBox.Row>
+      </Box> */}
     </Box>
-
-  
-  </Box>
   );
 };
