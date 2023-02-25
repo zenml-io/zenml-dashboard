a025a4e798aab2f69e470db984d4e3e786fa4534
talhainvenxion <talha@invenxion.com>
2023-02-26 03:38:39 +0500

    working on edit config

--- src/ui/layouts/stackComponents/StackDetail/Configuration/index.tsx
+++ src/ui/layouts/stackComponents/StackDetail/Configuration/index.tsx
@@ -9,32 +9,146 @@ import {
 } from '../../../../components';
 import styles from './index.module.scss';
 import { useService } from './useService';
+import axios from 'axios';
+import { useDispatch, useSelector } from '../../../../hooks';
+import {
+  sessionSelectors,
+  userSelectors,
+  workspaceSelectors,
+} from '../../../../../redux/selectors';
+import { showToasterAction } from '../../../../../redux/actions';
+import { toasterTypes } from '../../../../../constants';
 
 export const Configuration: React.FC<{ stackId: TId }> = ({ stackId }) => {
   const { stackComponent, flavor } = useService({
     stackId,
   });
-  console.log(
-    stackComponent?.configuration,
-    'flavoflavorr',
-    flavor?.config_schema?.properties,
-  );
-
+  const user = useSelector(userSelectors.myUser);
+  const authToken = useSelector(sessionSelectors.authenticationToken);
+  const selectedWorkspace = useSelector(workspaceSelectors.selectedWorkspace);
+  const dispatch = useDispatch();
+  const workspaces = useSelector(workspaceSelectors.myWorkspaces);
   const titleCase = (s: any) =>
     s.replace(/^_*(.)|_+(.)/g, (s: any, c: string, d: string) =>
       c ? c.toUpperCase() : ' ' + d.toUpperCase(),
     );
+
+  const onCallApi = (updateConfig: any) => {
+    // debugger;
+    const { id }: any = workspaces.find(
+      (item) => item.name === selectedWorkspace,
+    );
+
+    const body = {
+      user: user?.id,
+      workspace: id,
+      is_shared: updateConfig.isShared,
+      name: updateConfig.name,
+      type: updateConfig.type,
+      flavor: updateConfig.flavor,
+      configuration: updateConfig.configuration,
+    };
+
+    axios
+      .put(
+        `${process.env.REACT_APP_BASE_API_URL}/components/${updateConfig.id}`,
+        // @ts-ignore
+        body,
+        { headers: { Authorization: `Bearer ${authToken}` } },
+      )
+      .then((response: any) => {
+        // const id = response.data.id;
+
+        // setLoading(false);
+        dispatch(
+          showToasterAction({
+            description: 'Component has been updated successfully.',
+            type: toasterTypes.success,
+          }),
+        );
+        // dispatchStackData(1, 10);
+        // history.push(routePaths.stacks.base);
+        // dispatchStackComponentsData(1, 10);
+
+        // history.push(
+        //   routePaths.stackComponents.configuration(
+        //     flavor.type,
+        //     id,
+        //     selectedWorkspace,
+        //   ),
+        // );
+      })
+      .catch((err) => {
+        // debugger;
+        // setLoading(false);
+        // dispatch(
+        //   showToasterAction({
+        //     description: err?.response?.data?.detail[0],
+        //     type: toasterTypes.failure,
+        //   }),
+        // );
+      });
+  };
+  const onPressEnter = (
+    event?: any,
+    type?: string,
+    elementName?: any,
+    defaultValue?: any,
+    index?: any,
+  ) => {
+    if (event.key === 'Enter') {
+      // debugger;
+      const updateConfig = {
+        ...stackComponent,
+      };
+      if (type === 'string') {
+        updateConfig.configuration[elementName] = event.target.value;
+      }
+      if (type === 'name') {
+        updateConfig.name = event.target.value;
+      }
+      if (type === 'key') {
+        updateConfig.configuration[elementName][event.target.value] =
+          updateConfig.configuration[elementName][defaultValue];
+        delete updateConfig.configuration[elementName][defaultValue];
+      }
+      if (type === 'value') {
+        var unkownKey = Object.keys(updateConfig.configuration[elementName])[
+          index
+        ];
+        // debugger;
+        updateConfig.configuration[elementName][unkownKey] = event.target.value;
+        // delete updateConfig.configuration[elementName][defaultValue];
+      }
+
+      onCallApi(updateConfig);
+    }
+  };
+
+  const onChangeToggle = (value: any, type?: any, key?: any) => {
+    const updateConfig = {
+      ...stackComponent,
+    };
+    // debugger;
+    if (type === 'share') {
+      updateConfig.isShared = value;
+    }
+    if (type === 'other') {
+      updateConfig.configuration[key] = value;
+    }
+    onCallApi(updateConfig);
+  };
 
   const getFormElement: any = (elementName: any, elementSchema: any) => {
     if (typeof elementSchema === 'string') {
       return (
         <Box marginVertical={'md'} style={{ width: '100%' }}>
           <EditField
-            disabled
-            onChangeText={() => console.log('')}
+            onKeyDown={(e: any) => onPressEnter(e, 'string', elementName)}
+            onChangeText={(e: any) => onPressEnter(e, 'string', elementName)}
             label={titleCase(elementName)}
             optional={false}
-            value={elementSchema}
+            defaultValue={elementSchema}
             placeholder=""
             hasError={false}
             className={styles.field}
@@ -51,8 +165,9 @@ export const Configuration: React.FC<{ stackId: TId }> = ({ stackId }) => {
           {Object.keys(elementSchema).length < 1 && (
             <FlexBox.Row>
               <EditField
-                disabled
-                onChangeText={() => console.log('')}
+                // disabled
+                onKeyDown={onPressEnter}
+                onChangeText={(e: any) => {}}
                 label="Key"
                 optional={false}
                 value={''}
@@ -62,7 +177,7 @@ export const Configuration: React.FC<{ stackId: TId }> = ({ stackId }) => {
               />
               <div style={{ width: '10%' }}></div>
               <EditField
-                disabled
+                // disabled
                 // marginRight={'md'}
                 onChangeText={() => console.log('')}
                 label="Value"
@@ -74,26 +189,36 @@ export const Configuration: React.FC<{ stackId: TId }> = ({ stackId }) => {
               />
             </FlexBox.Row>
           )}
-          {Object.entries(elementSchema).map(([key, value]) => (
+          {Object.entries(elementSchema).map(([key, value], index) => (
             <FlexBox.Row>
               <EditField
-                disabled
-                onChangeText={() => console.log('')}
+                // disabled
+                onKeyDown={(e: any) => onPressEnter(e, 'key', elementName, key)}
+                onChangeText={(e: any) =>
+                  onPressEnter(e, 'key', elementName, key, index)
+                }
                 label="Key"
                 optional={false}
-                value={key}
+                defaultValue={key}
+                // value={key}
                 placeholder=""
                 hasError={false}
                 className={styles.field}
               />
               <div style={{ width: '10%' }}></div>
               <EditField
-                disabled
+                // disabled
                 // marginRight={'md'}
-                onChangeText={() => console.log('')}
+                onKeyDown={(e: any) =>
+                  onPressEnter(e, 'value', elementName, key, index)
+                }
+                onChangeText={(e: any) =>
+                  onPressEnter(e, 'value', elementName, key, index)
+                }
                 label="Value"
                 // optional={true}
-                value={value}
+                defaultValue={value}
+                // value={value}
                 placeholder=""
                 hasError={false}
                 className={styles.field}
@@ -107,10 +232,17 @@ export const Configuration: React.FC<{ stackId: TId }> = ({ stackId }) => {
       return (
         <Box marginVertical={'md'} style={{ width: '100%' }}>
           <Box>
+            {console.log(elementSchema, elementName, 'asdasdasda2222sdasd')}
             <FlexBox.Row justifyContent="space-between">
               <Paragraph>{titleCase(elementName)}</Paragraph>
               <label className={styles.switch}>
-                <input type="checkbox" checked={elementSchema} />
+                <input
+                  type="checkbox"
+                  defaultChecked={elementSchema}
+                  onChange={() =>
+                    onChangeToggle(!elementSchema, 'other', elementName)
+                  }
+                />
                 <span className={`${styles.slider} ${styles.round}`}></span>
               </label>
             </FlexBox.Row>
@@ -151,11 +283,13 @@ export const Configuration: React.FC<{ stackId: TId }> = ({ stackId }) => {
         <Container>
           <Box style={{ width: '79%' }}>
             <EditField
-              disabled
-              onChangeText={() => console.log('')}
-              label={'Flavor Name'}
+              // disabled
+              onKeyDown={(e: any) => onPressEnter(e, 'name')}
+              onChangeText={(e: any) => onPressEnter(e, 'name')}
+              label={'Component Name'}
               optional={false}
-              value={stackComponent.flavor}
+              defaultValue={stackComponent.name}
+              // value={stackComponent.name}
               placeholder=""
               hasError={false}
               className={styles.field}
@@ -166,7 +300,15 @@ export const Configuration: React.FC<{ stackId: TId }> = ({ stackId }) => {
           <FlexBox.Row justifyContent="space-between" style={{ width: '70%' }}>
             <Paragraph>Share Component with public</Paragraph>
             <label className={styles.switch}>
-              <input type="checkbox" checked={stackComponent.isShared} />
+              {console.log(stackComponent, 'asdasdasda2222122sdasd')}
+              <input
+                type="checkbox"
+                defaultChecked={stackComponent.isShared}
+                // checked={stackComponent.isShared}
+                onChange={() =>
+                  onChangeToggle(!stackComponent.isShared, 'share')
+                }
+              />
               <span className={`${styles.slider} ${styles.round}`}></span>
             </label>
           </FlexBox.Row>
