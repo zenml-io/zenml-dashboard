a19c1bea2fd68ca3a26e7316e0351b432b184938
talhainvenxion <talha@invenxion.com>
2023-03-09 03:16:08 +0500

    handle expended for stack Components

--- src/ui/layouts/stackComponents/RunsTable/index.tsx
+++ src/ui/layouts/stackComponents/RunsTable/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { routePaths } from '../../../../routes/routePaths';
 import { useHistory, useLocationPath, useSelector } from '../../../hooks';
 
@@ -7,6 +7,11 @@ import { Table } from '../../common/Table';
 import { useHeaderCols } from './HeaderCols';
 import { useService } from './useService';
 import { workspaceSelectors } from '../../../../redux/selectors';
+import { FlexBox, If } from '../../../components';
+import { ItemPerPage } from '../../common/ItemPerPage';
+import { Pagination } from '../../common/Pagination';
+import { usePaginationAsQueryParam } from '../../../hooks/usePaginationAsQueryParam';
+import { callActionForStackComponentRunsForPagination } from '../StackDetail/useService';
 
 export const RunsTable: React.FC<{
   isExpended?: boolean;
@@ -34,7 +39,19 @@ export const RunsTable: React.FC<{
   const history = useHistory();
   const locationPath = useLocationPath();
   const selectedWorkspace = useSelector(workspaceSelectors.selectedWorkspace);
-  // debugger;
+  const { pageIndex, setPageIndex } = usePaginationAsQueryParam();
+  const {
+    dispatchStackComponentRunsData,
+  } = callActionForStackComponentRunsForPagination();
+  const ITEMS_PER_PAGE = parseInt(
+    process.env.REACT_APP_ITEMS_PER_PAGE as string,
+  );
+  const DEFAULT_ITEMS_PER_PAGE = 10;
+  const [itemPerPage, setItemPerPage] = useState(
+    ITEMS_PER_PAGE ? ITEMS_PER_PAGE : DEFAULT_ITEMS_PER_PAGE,
+  );
+  const initialRef: any = null;
+  const childRef = React.useRef(initialRef);
   const {
     sortedRuns,
     setSortedRuns,
@@ -44,7 +61,12 @@ export const RunsTable: React.FC<{
     setActiveSortingDirection,
     setSelectedRunIds,
   } = useService({ runIds, filter });
-
+  const validFilters = filter?.filter((item: any) => item.value);
+  const isValidFilterFroValue: any = filter?.map((f: any) => f.value).join('');
+  const isValidFilterForCategory: any = filter
+    ?.map((f: any) => f.value && f.type.value)
+    .join('');
+  const checkValidFilter = isValidFilterFroValue + isValidFilterForCategory;
   const openDetailPage = (run: TRun) => {
     setSelectedRunIds([]);
 
@@ -99,30 +121,89 @@ export const RunsTable: React.FC<{
     activeSortingDirection,
     setActiveSortingDirection,
   });
-
   useEffect(() => {
-    getSorted(activeSorting, activeSortingDirection);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [getSorted]);
+    if (filter) {
+      setPageIndex(0);
+    }
+    dispatchStackComponentRunsData(
+      stackComponentId,
+      1,
+      itemPerPage,
+      checkValidFilter.length ? (validFilters as any) : [],
+      (activeSortingDirection?.toLowerCase() + ':' + activeSorting) as any,
+    );
+  }, [checkValidFilter, activeSortingDirection, activeSorting]);
+  const onChange = (pageNumber: any, size: any) => {
+    // debugger;
+    dispatchStackComponentRunsData(
+      stackComponentId,
+
+      pageNumber,
+      size,
+      checkValidFilter.length ? (validFilters as any) : [],
+      (activeSortingDirection?.toLowerCase() + ':' + activeSorting) as any,
+    );
+  };
   return (
-    <Table
-      activeSorting={
-        activeSortingDirection?.toLowerCase() + ':' + activeSorting
-      }
-      // activeSorting={
-      //   activeSorting !== 'created' && activeSortingDirection !== 'ASC'
-      //     ? activeSorting
-      //     : 'created'
-      // }
-      pagination={pagination}
-      loading={fetching}
-      showHeader={true}
-      filters={filter}
-      paginated={paginated}
-      headerCols={headerCols}
-      tableRows={sortedRuns}
-      emptyState={{ text: emptyStateText }}
-      trOnClick={openDetailPage}
-    />
+    <>
+      <Table
+        activeSorting={
+          activeSortingDirection?.toLowerCase() + ':' + activeSorting
+        }
+        // activeSorting={
+        //   activeSorting !== 'created' && activeSortingDirection !== 'ASC'
+        //     ? activeSorting
+        //     : 'created'
+        // }
+        pagination={pagination}
+        loading={fetching}
+        showHeader={true}
+        filters={filter}
+        paginated={paginated}
+        headerCols={headerCols}
+        tableRows={sortedRuns}
+        emptyState={{ text: emptyStateText }}
+        trOnClick={openDetailPage}
+      />
+      <If condition={sortedRuns.length > 0}>
+        {() => (
+          <FlexBox
+            marginTop="xxxl"
+            marginBottom="xxxl"
+            style={{ alignSelf: 'center' }}
+            justifyContent="center"
+          >
+            <Pagination
+              // isExpended={isExpended}
+              ref={childRef}
+              onChange={(pageNumber: any) => onChange(pageNumber, itemPerPage)}
+              // getFetchedState={getFetchedState}
+              activeSorting={activeSorting}
+              filters={filter}
+              itemPerPage={itemPerPage}
+              pageIndex={pageIndex}
+              setPageIndex={setPageIndex}
+              pages={paginated?.totalPages}
+              totalOfPages={paginated?.totalPages}
+              totalLength={paginated?.length}
+              totalCount={paginated?.totalitem}
+            />
+
+            <If condition={sortedRuns.length > 0 && paginated?.totalitem > 1}>
+              {() => (
+                <ItemPerPage
+                  itemPerPage={itemPerPage}
+                  onChangePagePerItem={(size: any) => {
+                    setItemPerPage(size);
+                    onChange(1, size);
+                    setPageIndex(0);
+                  }}
+                ></ItemPerPage>
+              )}
+            </If>
+          </FlexBox>
+        )}
+      </If>
+    </>
   );
 };
