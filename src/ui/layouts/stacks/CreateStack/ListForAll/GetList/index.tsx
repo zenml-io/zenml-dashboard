e1c14b7477df53e67dd09b532ac1bf676bb4f741
talhainvenxion <talha@invenxion.com>
2023-03-11 18:58:00 +0500

    fixed

--- src/ui/layouts/stacks/CreateStack/ListForAll/GetList/index.tsx
+++ src/ui/layouts/stacks/CreateStack/ListForAll/GetList/index.tsx
@@ -47,7 +47,7 @@ export const GetList: React.FC<Props> = ({
             : locationPath.split('/')[2],
           type: type,
           page: 1,
-          size: 50,
+          size: 1000,
 
           onSuccess: (res) => {
             const updatedList = res.items.map((item: any) => {
