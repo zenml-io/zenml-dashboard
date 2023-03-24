1e40743c81daaf1d2dedf7eefc8f730245c23463
mohammad-alizia <58190992+mohammad-alizia@users.noreply.github.com>
2023-03-25 02:32:42 +0500

    linting resolved

--- src/ui/layouts/runs/RunDetail/Detail/index.tsx
+++ src/ui/layouts/runs/RunDetail/Detail/index.tsx
@@ -4,13 +4,13 @@ import {
   FlexBox,
 } from '../../../../components';
 
-import { useService } from './useService';
+// import { useService } from './useService';
 // import {setRunDetails} from '../../../../../redux/actions/runs/setRunDetails'
-import { useDispatch } from 'react-redux';
+// import { useDispatch } from 'react-redux';
 import JsonDisplay from '../../../../components/lineage/JsonDisplay';
 export const Details: React.FC<{ runId: TId }> = ({ runId }) => {
   // const dispatch = useDispatch();
-  const { run } = useService({ runId });
+  // const { run } = useService({ runId });
 
   const artifact: any = localStorage.getItem("__ARTIFACT");
   const step: any = localStorage.getItem("__STEP");
