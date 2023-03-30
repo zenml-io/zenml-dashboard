0684fde3448ad4b531c05436bfb53012dc44bf86
Sam Woolerton <samuelwoolerton@gmail.com>
2023-03-30 13:12:00 +1300

    User plugins page

--- src/ui/layouts/settings/Plugins.tsx
+++ src/ui/layouts/settings/Plugins.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import moment from 'moment';
 import Lottie from 'lottie-react';
 
 import { selectedWorkspace } from '../../../redux/selectors';
@@ -12,17 +13,14 @@ import loadingAnimation from './loadingAnimation.json';
 import { HUB_API_URL } from '../../../api/constants';
 import { useHubToken } from '../../hooks/auth';
 
-const pending = [{ name: 'Plugin name', status: 'Building wheels' }];
-const completed = [
-  { month: 'March 2023', plugins: [...new Array(4).fill(null)] },
-  { month: 'February 2023', plugins: [...new Array(1).fill(null)] },
-];
-
-const getData = async (token: string) => {
+const getData = async (token: string, status: 'pending' | 'available') => {
   return (
-    await axios.get(`${HUB_API_URL}/plugins?only_latest=true&mine=true`, {
-      headers: { Authorization: `Bearer ${token}` },
-    })
+    await axios.get(
+      `${HUB_API_URL}/plugins?only_latest=true&status=${status}&mine=true`,
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      },
+    )
   ).data as TPlugin[];
 };
 
@@ -30,32 +28,48 @@ export const Plugins: React.FC = () => {
   const workspace = useSelector(selectedWorkspace);
   const history = useHistory();
   const token = useHubToken();
-  const [plugins, setPlugins] = useState([] as TPlugin[]);
+  const [pendingPlugins, setPendingPlugins] = useState([] as TPlugin[]);
+  const [completedPlugins, setCompletedPlugins] = useState(
+    [] as { month: string; plugins: TPlugin[] }[],
+  );
 
   useEffect(() => {
     // shouldn't be possible
     if (!token) return;
 
-    getData(token).then((p) => {
-      setPlugins(p);
-      console.log('PLUGINS ARE', p);
+    getData(token, 'pending').then(setPendingPlugins);
+    getData(token, 'available').then((plugins) => {
+      const grouped = plugins.reduce((acc, next) => {
+        const month = moment(next.created).format('MMMM yyyy');
+        if (!acc[month]) acc[month] = [];
+        acc[month].push(next);
+        return acc;
+      }, {} as Record<string, TPlugin[]>);
+
+      const ls = Object.entries(grouped).map(([month, plugins]) => ({
+        month,
+        plugins,
+      }));
+
+      setCompletedPlugins(ls);
     });
-    // getData(token).then(setPlugins);
   }, []);
 
   return (
     <Box>
-      {pending.length > 0 && (
+      {pendingPlugins.length > 0 && (
         <Box marginVertical="lg">
           <Paragraph color="darkGrey" size="small">
             Pending activity
           </Paragraph>
 
           <Box marginVertical="lg">
-            {pending.map((p, i) => (
+            {pendingPlugins.map((p, i) => (
               <LinkBox
                 key={i}
-                onClick={() => alert("TODO: page isn't built yet")}
+                onClick={() =>
+                  history.push(routePaths.plugins.creationLogs(workspace, p.id))
+                }
                 style={{
                   display: 'flex',
                   width: '100%',
@@ -82,7 +96,9 @@ export const Plugins: React.FC = () => {
                     color="grey"
                     style={{ fontSize: '20px', marginTop: '8px' }}
                   >
-                    {p.status}
+                    Pending
+                    {/* TODO: get the last line of build logs for status? */}
+                    {/* {p.build_logs?.split('\n') ?? 'Pending'} */}
                   </Paragraph>
                 </Box>
 
@@ -104,8 +120,8 @@ export const Plugins: React.FC = () => {
 
       <Box marginVertical="xl">
         <Paragraph color="primary">Completed</Paragraph>
-        {completed.length > 0 ? (
-          completed.map((m) => (
+        {completedPlugins.length > 0 ? (
+          completedPlugins.map((m) => (
             <Box key={m.month} marginVertical="lg">
               <Paragraph
                 size="tiny"
