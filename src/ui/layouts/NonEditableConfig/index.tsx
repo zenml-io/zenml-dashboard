8d5082805e3ab9d820d827d44bbd68d714a79aee
talhainvenxion <talha@invenxion.com>
2023-04-07 03:45:41 +0500

    fixed

--- src/ui/layouts/NonEditableConfig/index.tsx
+++ src/ui/layouts/NonEditableConfig/index.tsx
@@ -138,42 +138,52 @@ export const NonEditableConfig: React.FC<{ details: any }> = ({ details }) => {
   }
   // const values = [...flavor?.config_schema?.properties];
 
-  let result = Object.keys(flavor?.config_schema?.properties).reduce(function (
-    r: any,
-    name: any,
-  ) {
-    return (
-      (r[name] =
-        flavor?.config_schema?.properties[name].type === 'string' &&
-        flavor?.config_schema?.properties[name].default === undefined
-          ? ''
-          : flavor?.config_schema?.properties[name].default),
-      r
-    );
-  },
-  {});
-  let normalizeConfiguration = Object.keys(details?.configuration).reduce(
-    function (r: any, name: any) {
-      if (details?.configuration[name] === null) {
-        return (
-          (r[name] =
-            details?.configuration[name] === null &&
-            flavor?.config_schema?.properties[name].default === undefined
-              ? ''
-              : flavor?.config_schema?.properties[name].default),
-          r
-        );
-      } else {
-        return {};
+  // let result = Object.keys(flavor?.config_schema?.properties).reduce(function (
+  //   r: any,
+  //   name: any,
+  // ) {
+  //   return (
+  //     (r[name] =
+  //       flavor?.config_schema?.properties[name].type === 'string' &&
+  //       flavor?.config_schema?.properties[name].default === undefined
+  //         ? ''
+  //         : flavor?.config_schema?.properties[name].default),
+  //     r
+  //   );
+  // },
+  // {});
+  // let normalizeConfiguration = Object.keys(details?.configuration).reduce(
+  //   function (r: any, name: any) {
+  //     if (details?.configuration[name] === null) {
+  //       return (
+  //         (r[name] =
+  //           details?.configuration[name] === null &&
+  //           flavor?.config_schema?.properties[name].default === undefined
+  //             ? ''
+  //             : flavor?.config_schema?.properties[name].default),
+  //         r
+  //       );
+  //     } else {
+  //       return {};
+  //     }
+  //   },
+  //   {},
+  // );
+  function replaceNullWithEmptyString(obj: any) {
+    for (let prop in obj) {
+      if (obj[prop] === null) {
+        obj[prop] = '';
+      } else if (typeof obj[prop] === 'object') {
+        replaceNullWithEmptyString(obj[prop]);
       }
-    },
-    {},
-  );
+    }
+    return obj;
+  }
+
+  replaceNullWithEmptyString(details?.configuration);
 
   const mappedObject = {
-    ...result,
     ...details?.configuration,
-    ...normalizeConfiguration,
   };
 
   return (
