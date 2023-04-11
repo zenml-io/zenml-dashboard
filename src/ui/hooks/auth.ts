ba024c3e8582fba409751fbcac2b4ff3a505104d
Sam Woolerton <samuelwoolerton@gmail.com>
2023-04-12 11:55:35 +1200

    Check token for expiry and remove it if it fails

--- src/ui/hooks/auth.ts
+++ src/ui/hooks/auth.ts
@@ -1,15 +1,34 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { sessionSelectors } from '../../redux/selectors';
 import { HUB_API_URL } from '../../api/constants';
+import { useToaster } from './useToaster';
+import { disconnectHubActionTypes } from '../../redux/actionTypes';
 
 export const useAuthToken = (): string | null =>
   useSelector(sessionSelectors.authenticationToken);
 
-export const useHubToken = (): string | null =>
-  useSelector(sessionSelectors.hubToken);
+export const useHubToken = (): string | null => {
+  const hubToken = useSelector(sessionSelectors.hubToken);
+  const { failureToast } = useToaster();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!hubToken) return;
+
+    getUserDetails(hubToken).catch(() => {
+      failureToast({
+        description: 'Hub token has expired; please reconnect in settings',
+      });
+
+      dispatch({ type: disconnectHubActionTypes.success });
+    });
+  }, []);
+
+  return hubToken;
+};
 
 export const useHubUser = (): THubUser | undefined => {
   const [user, setUser] = useState(undefined as THubUser | undefined);
@@ -18,13 +37,16 @@ export const useHubUser = (): THubUser | undefined => {
   useEffect(() => {
     if (user || !hubToken) return;
 
-    axios
-      .get(`${HUB_API_URL}/users/me`, {
-        headers: { Authorization: `Bearer ${hubToken}` },
-      })
-      .then((res) => setUser(res.data));
+    getUserDetails(hubToken).then((res) => setUser(res.data));
+
     // eslint-disable-next-line
   }, [hubToken]);
 
   return user;
 };
+
+function getUserDetails(hubToken: string) {
+  return axios.get(`${HUB_API_URL}/users/me`, {
+    headers: { Authorization: `Bearer ${hubToken}` },
+  });
+}
