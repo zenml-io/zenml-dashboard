d65761d93fea77794bf7de6081952926e54c49b5
talhainvenxion <talha@invenxion.com>
2023-04-20 06:03:53 +0500

    fixed

--- src/ui/layouts/stackComponents/UpdateComponent/UpdateConfig/index.tsx
+++ src/ui/layouts/stackComponents/UpdateComponent/UpdateConfig/index.tsx
@@ -1048,12 +1048,12 @@ export const UpdateConfig: React.FC<{
               //   routePaths.secret.updateSecret(secret.id, selectedWorkspace),
               // )
             }
-            style={{
-              background: '#FFFFFF',
-              boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.05)',
-              borderRadius: '4px',
-              color: '#443E99',
-            }}
+            // style={{
+            //   background: '#FFFFFF',
+            //   boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.05)',
+            //   borderRadius: '4px',
+            //   color: '#443E99',
+            // }}
           >
             Save Changes
           </PrimaryButton>
