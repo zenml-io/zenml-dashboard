1ef8b63ae4b917b70f412deb4484b9a915063dd8
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-05-09 15:11:08 +0000

    feat: add requirements and dockerfile

--- src/ui/components/runDetailCards/Cards/OrchestratorCard.tsx
+++ src/ui/components/runDetailCards/Cards/OrchestratorCard.tsx
@@ -8,6 +8,7 @@ interface OrchestratorCardProps {
 }
 
 const OrchestratorCard = ({ run }: OrchestratorCardProps) => {
+  console.log(run);
   return (
     <DetailCard isInitiallyOpen={true} heading="Orchestrator">
       <div>
@@ -44,6 +45,52 @@ const OrchestratorCard = ({ run }: OrchestratorCardProps) => {
             <Paragraph className={styles.card__value}>
               {/* @ts-ignore */}
               {run.build?.images?.orchestrator?.image}
+            </Paragraph>
+          </div>
+          {/* @ts-ignore */}
+          {run.build?.images?.orchestrator?.dockerfile ? (
+            <div>
+              <details>
+                <summary className={styles.card__summary}>
+                  <Paragraph
+                    style={{ display: 'inline' }}
+                    className={styles.card__key}
+                  >
+                    Dockerfile
+                  </Paragraph>
+                </summary>
+                <div className={styles.card__detailsContainer}>
+                  {/* @ts-ignore */}
+                  <Paragraph
+                    style={{ whiteSpace: 'pre-wrap' }}
+                    className={styles.card__value}
+                  >
+                    {/* @ts-ignore */}
+                    {run.build?.images?.orchestrator?.dockerfile || 'n/a'}
+                  </Paragraph>
+                </div>
+              </details>
+            </div>
+          ) : (
+            <div>
+              <Paragraph className={styles.card__key}>Dockerfile</Paragraph>
+              <Paragraph
+                style={{ whiteSpace: 'pre-wrap' }}
+                className={styles.card__value}
+              >
+                n/a
+              </Paragraph>
+            </div>
+          )}
+
+          <div>
+            <Paragraph className={styles.card__key}>Requirements</Paragraph>
+            <Paragraph
+              style={{ whiteSpace: 'pre-wrap' }}
+              className={styles.card__value}
+            >
+              {/* @ts-ignore */}
+              {run.build?.images?.orchestrator?.requirements || 'n/a'}
             </Paragraph>
           </div>
           <div>
