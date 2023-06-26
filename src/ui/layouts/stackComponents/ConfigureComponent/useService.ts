779ca189e9586b8ceed49da76b51f16941ff1581
talhainvenxion <talha@invenxion.com>
2023-06-26 18:04:06 +0500

    Now it is calling the resources endpoint only once.

--- src/ui/layouts/stackComponents/ConfigureComponent/useService.ts
+++ src/ui/layouts/stackComponents/ConfigureComponent/useService.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { flavorsActions, flavorPagesActions } from '../../../../redux/actions';
 // import { workspaceSelectors } from '../../../../redux/selectors';
 import {
@@ -9,9 +9,11 @@ import {
   useParams,
   useSelector,
 } from '../../../hooks';
+import axios from 'axios';
 import { DEFAULT_WORKSPACE_NAME } from '../../../../constants';
 import {
   flavorSelectors,
+  sessionSelectors,
   workspaceSelectors,
 } from '../../../../redux/selectors';
 import { filterObjectForParam } from '../../../../utils';
@@ -20,14 +22,45 @@ import { FlavorDetailRouteParams } from '.';
 interface ServiceInterface {
   flavor: any;
   id: any;
+  fetching?: boolean;
+  serviceConnectorResources?: any;
 }
 
 export const useService = (): ServiceInterface => {
   const { id } = useParams<FlavorDetailRouteParams>();
   const flavor: any = useSelector(flavorSelectors.flavorForId(id));
+  const authToken = useSelector(sessionSelectors.authenticationToken);
+  const [fetching, setFetching] = useState(false);
+  const selectedWorkspace = useSelector(workspaceSelectors.selectedWorkspace);
+  const [serviceConnectorResources, setServiceConnectorResources] = useState(
+    [],
+  );
+  useEffect(() => {
+    if (flavor.connectorResourceType) {
+      fetchResourcesList();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [flavor.connectorResourceType]);
 
+  const fetchResourcesList = async () => {
+    setFetching(true);
+    const response = await axios.get(
+      `${process.env.REACT_APP_BASE_API_URL}/workspaces/${selectedWorkspace}/service_connectors/resources?resource_type=${flavor.connectorResourceType}`,
+      {
+        headers: {
+          Authorization: `bearer ${authToken}`,
+        },
+      },
+    );
+
+    setServiceConnectorResources(response?.data);
+    setFetching(false);
+    //Setting the response into state
+  };
   return {
     id,
     flavor,
+    fetching,
+    serviceConnectorResources,
   };
 };
