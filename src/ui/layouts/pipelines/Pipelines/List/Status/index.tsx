ba3763729864e6e8e2acc91b3aee10e7b8d17a99
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-07-04 07:46:48 +0000

    fix: status

--- src/ui/layouts/pipelines/Pipelines/List/Status/index.tsx
+++ src/ui/layouts/pipelines/Pipelines/List/Status/index.tsx
@@ -1,156 +1,108 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
 import ReactTooltip from 'react-tooltip';
 import { runStatus, iconColors, iconSizes } from '../../../../../../constants';
-import { routePaths } from '../../../../../../routes/routePaths';
 
-import {
-  Box,
-  // ColoredCircle,
-  FlexBox,
-  icons,
-  If,
-  Paragraph,
-} from '../../../../../components';
-import { useService } from './useService';
-import { useSelector } from '../../../../../hooks';
-import { workspaceSelectors } from '../../../../../../redux/selectors';
+import { Box, FlexBox, icons, If, Paragraph } from '../../../../../components';
+
+function getID(pipelineID: string, index: number) {
+  return `${pipelineID}-${index}`;
+}
 
 export const Status: React.FC<{ pipeline: TPipeline }> = ({ pipeline }) => {
-  const { lastThreeRuns } = useService({ pipeline });
-
-  const history = useHistory();
-  const selectedWorkspace = useSelector(workspaceSelectors.selectedWorkspace);
   return (
     <FlexBox
       alignItems="center"
       justifyContent="center"
       style={{ marginLeft: '-16px' }}
     >
-      {lastThreeRuns.map((item: any, index: number) => (
+      {pipeline.status.map((item: any, index: number) => (
         <Box key={index}>
           <>
             <FlexBox alignItems="center">
-              <div data-tip data-for={item.status}>
-                <If condition={item.status === runStatus.COMPLETED}>
+              <div data-tip data-for={getID(pipeline.id, index)}>
+                <If condition={item === runStatus.COMPLETED}>
                   {() => (
-                    <div
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        history.push(
-                          routePaths.run.pipeline.statistics(
-                            selectedWorkspace,
-                            item.run.id,
-                            pipeline.id,
-                          ),
-                        );
-                      }}
-                    >
-                      {/* <ColoredCircle color="green" size="xs"> */}
+                    <div>
                       <icons.circleCheck
                         color={iconColors.lightGreen}
                         size={iconSizes.md}
                       />
-                      {/* </ColoredCircle> */}
                     </div>
                   )}
                 </If>
               </div>
-              <ReactTooltip id={item.status} place="top" effect="solid">
-                <Paragraph color="white">{item.status}</Paragraph>
+              <ReactTooltip
+                id={getID(pipeline.id, index)}
+                place="top"
+                effect="solid"
+              >
+                <Paragraph color="white">{item}</Paragraph>
               </ReactTooltip>
             </FlexBox>
 
             <FlexBox alignItems="center">
-              <div data-tip data-for={item.status}>
-                <If condition={item.status === runStatus.RUNNING}>
+              <div data-tip data-for={getID(pipeline.id, index)}>
+                <If condition={item === runStatus.RUNNING}>
                   {() => (
-                    <div
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        history.push(
-                          routePaths.run.pipeline.statistics(
-                            selectedWorkspace,
-                            item.run.id,
-                            pipeline.id,
-                          ),
-                        );
-                      }}
-                    >
-                      {/* <ColoredCircle color="secondary" size="xs"> */}
+                    <div>
                       <icons.inProgress
                         color={iconColors.orange}
                         size={iconSizes.md}
                       />
-                      {/* </ColoredCircle> */}
                     </div>
                   )}
                 </If>
               </div>
-              <ReactTooltip id={item.runId} place="top" effect="solid">
-                <Paragraph color="white">{item.status}</Paragraph>
+              <ReactTooltip
+                id={getID(pipeline.id, index)}
+                place="top"
+                effect="solid"
+              >
+                <Paragraph color="white">{item}</Paragraph>
               </ReactTooltip>
             </FlexBox>
 
             <FlexBox alignItems="center">
-              <div data-tip data-for={item.runId}>
-                <If condition={item.status === runStatus.FAILED}>
+              <div data-tip data-for={getID(pipeline.id, index)}>
+                <If condition={item === runStatus.FAILED}>
                   {() => (
-                    <div
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        history.push(
-                          routePaths.run.pipeline.statistics(
-                            selectedWorkspace,
-                            item.run.id,
-                            pipeline.id,
-                          ),
-                        );
-                      }}
-                    >
-                      {/* <ColoredCircle color="red" size="xs"> */}
+                    <div>
                       <icons.failed
                         color={iconColors.red}
                         size={iconSizes.md}
                       />
-                      {/* </ColoredCircle> */}
                     </div>
                   )}
                 </If>
               </div>
-              <ReactTooltip id={item.runId} place="top" effect="solid">
-                <Paragraph color="white">{item.status}</Paragraph>
+              <ReactTooltip
+                id={getID(pipeline.id, index)}
+                place="top"
+                effect="solid"
+              >
+                <Paragraph color="white">{item}</Paragraph>
               </ReactTooltip>
             </FlexBox>
 
             <FlexBox alignItems="center">
-              <div data-tip data-for={item.runId}>
-                <If condition={item.status === runStatus.CACHED}>
+              <div data-tip data-for={getID(pipeline.id, index)}>
+                <If condition={item === runStatus.CACHED}>
                   {() => (
-                    <div
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        history.push(
-                          routePaths.run.pipeline.statistics(
-                            selectedWorkspace,
-                            item.run.id,
-                            pipeline.id,
-                          ),
-                        );
-                      }}
-                    >
-                      {/* <ColoredCircle color="mustard" size="xs"> */}
+                    <div>
                       <icons.cached
                         color={iconColors.butterflyBlue}
                         size={iconSizes.md}
                       />
-                      {/* </ColoredCircle> */}
                     </div>
                   )}
                 </If>
               </div>
-              <ReactTooltip id={item.runId} place="top" effect="solid">
-                <Paragraph color="white">{item.status}</Paragraph>
+              <ReactTooltip
+                id={getID(pipeline.id, index)}
+                place="top"
+                effect="solid"
+              >
+                <Paragraph color="white">{item}</Paragraph>
               </ReactTooltip>
             </FlexBox>
           </>
