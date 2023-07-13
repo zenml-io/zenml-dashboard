9dabd9ae65445f07dba1c954782c6bdcf56265dc
talhainvenxion <talha@invenxion.com>
2023-07-13 17:18:33 +0500

    fixed

--- src/ui/components/lineage/index.tsx
+++ src/ui/components/lineage/index.tsx
@@ -230,7 +230,13 @@ export const LayoutFlow: React.FC<any> = (graph: any) => {
               }}
               fitView
             >
-              <Controls style={{ top: '0px', bottom: '50%' }} />
+              <Controls
+                style={{
+                  top: '0px',
+                  bottom: '50%',
+                  boxShadow: '0px',
+                }}
+              />
             </ReactFlow>
           </div>
         </div>
