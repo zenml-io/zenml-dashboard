a4e2a9c70c395917f60d5e8b1ee2abb2fb477024
talhainvenxion <talha@invenxion.com>
2023-07-24 19:03:24 +0500

    user can start or stop polling for DAG visualzier.

--- src/ui/components/dag/index.tsx
+++ src/ui/components/dag/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useService } from './useService';
 import { LayoutFlow } from '../lineage';
 import { FullWidthSpinner } from '../spinners';
@@ -13,7 +13,8 @@ export const DAG: React.FC<{
   fetching?: boolean;
   metadata?: any;
 }> = ({ runId, fetching, metadata }) => {
-  const { graph } = useService({ runId });
+  const [togglePolling, setTogglePolling] = useState(false);
+  const { graph, counter } = useService({ runId, togglePolling });
 
   useEffect(() => {}, [runId]); //eslint-disable-line
 
@@ -25,6 +26,29 @@ export const DAG: React.FC<{
     <div style={styles.container}>
       <div style={styles.dag}>
         <LayoutFlow graph={graph} runId={runId} metadata={metadata} />
+        <button
+          style={{
+            color: '#443E99',
+            position: 'sticky',
+            bottom: '10px',
+            zIndex: 10,
+            fontFamily: 'Rubik',
+            fontWeight: 'bold',
+            fontSize: '16px',
+            width: '185px',
+            height: '51px',
+            backgroundColor: 'rgb(241, 240, 255)',
+            border: 'none',
+            padding: '0.75rem',
+            borderRadius: '0.5rem',
+            cursor: 'pointer',
+          }}
+          onClick={() => setTogglePolling(!togglePolling)}
+        >
+          {!togglePolling
+            ? 'Stop Auto Refresh ' + counter
+            : 'Start Auto Refresh'}
+        </button>
       </div>
     </div>
   );
--- src/ui/components/dag/useService.ts
+++ src/ui/components/dag/useService.ts
@@ -1,12 +1,48 @@
 // import { useEffect } from 'react';
-import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { runSelectors } from '../../../redux/selectors';
+import { runsActions } from '../../../redux/actions';
 
-export const useService = ({ runId }: { runId: TId }) => {
+export const useService = ({
+  runId,
+  togglePolling,
+}: {
+  runId: TId;
+  togglePolling: boolean;
+}) => {
+  const dispatch = useDispatch();
   const graph = useSelector(runSelectors.graphByRunId(runId));
+  const [counter, setCounter] = useState(10);
 
-  useEffect(() => {}, [graph]);
+  useEffect(() => {
+    if (togglePolling) {
+      setCounter(10);
+    }
+    if (!togglePolling) {
+      const counterInterval = setInterval(() => {
+        setCounter((prevCounter) => {
+          // Reset the counter to 1 after reaching 10
+          if (prevCounter === 0) return 10;
+          // Otherwise, continue incrementing
+          return prevCounter - 1;
+        });
+      }, 1000);
+      const intervalId = setInterval(() => {
+        dispatch(
+          runsActions.graphForRun({
+            runId: runId,
+          }),
+        );
+      }, 11000);
 
-  return { graph };
+      return () => {
+        clearInterval(intervalId);
+        clearInterval(counterInterval);
+      };
+    }
+    // This is important
+  }, [runId, dispatch, togglePolling]);
+
+  return { graph, counter };
 };
