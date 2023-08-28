9c6bb7304f63746c94b7993e285030ebe6d16b55
talhainvenxion <talha@invenxion.com>
2023-08-28 17:03:30 +0500

    Handle response types for stacks.

--- src/api/types.ts
+++ src/api/types.ts
@@ -2,3 +2,18 @@ import { components } from '../../schema';
 
 //Pipelines
 export type Pipeline = components['schemas']['PipelineResponseModel'];
+
+//stacks
+export type Stack = components['schemas']['StackResponseModel'];
+
+// Flavor
+export type Flavor = components['schemas']['FlavorResponseModel'];
+
+//components
+export type StackComponent = components['schemas']['ComponentResponseModel'];
+
+// //service connector
+// export type ServiceConnector = components['schemas']['ServiceConnectorResponseModel'];
+
+// //Repository
+// export type CodeRepository = components['schemas']['CodeRepositoryResponseModel'];
--- src/ui/layouts/stacks/Stacks/List/Status/useService.ts
+++ src/ui/layouts/stacks/Stacks/List/Status/useService.ts
@@ -1,12 +1,13 @@
 import { runSelectors } from '../../../../../../redux/selectors';
 import { getLastThreeRuns } from '../../../../../../utils';
 import { useSelector } from '../../../../../hooks';
+import { Stack } from '../../../../../../api/types';
 
 interface ServiceInterface {
   lastThreeRuns: TRun[];
 }
 
-export const useService = ({ stack }: { stack: TStack }): ServiceInterface => {
+export const useService = ({ stack }: { stack: Stack }): ServiceInterface => {
   const runs = useSelector(runSelectors.runsForPipelineId(stack.id));
 
   const lastThreeRuns = getLastThreeRuns(runs);
