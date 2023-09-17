a4e5a939797f7c6e66aad2ed5f65232934b3827e
talhainvenxion <talha@invenxion.com>
2023-09-18 01:42:09 +0500

    change action for runs list

--- src/redux/actionTypes/runPages.ts
+++ src/redux/actionTypes/runPages.ts
@@ -2,5 +2,5 @@ import { actionTypes } from './constants';
 
 export const runPagesActionTypes = {
   setSelectedRunIds: actionTypes.PIPELINE_PAGES_SET_SELECTED_RUN_IDS,
-  setFetching: actionTypes.PIPELINE_PAGES_SET_FETCHING,
+  setFetching: actionTypes.RUNS_PAGES_SET_FETCHING,
 };
