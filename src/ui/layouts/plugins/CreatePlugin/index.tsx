dfe40e1fea5f124508c87de1d70ea7ad6b294b7a
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-09-25 14:50:36 +0200

    fix: build

--- src/ui/layouts/plugins/CreatePlugin/index.tsx
+++ src/ui/layouts/plugins/CreatePlugin/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import axios from 'axios';
 import moment from 'moment';
 
 import {
--- src/ui/layouts/plugins/UpdatePlugin/index.tsx
+++ src/ui/layouts/plugins/UpdatePlugin/index.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import { useHistory, useParams } from 'react-router-dom';
 import moment from 'moment';
 
