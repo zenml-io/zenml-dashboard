0abfd482bfe04d9e39e40477ade68f0dafdb0339
talhainvenxion <110528564+talhainvenxion@users.noreply.github.com>
2023-09-25 15:26:46 +0500

    remove polling from runs (all-runs and pipeline runs) (#497)

--- src/ui/layouts/pipelines/PipelineDetail/Runs/useService.ts
+++ src/ui/layouts/pipelines/PipelineDetail/Runs/useService.ts
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { pipelinesActions } from '../../../../../redux/actions';
+import { useSelector } from 'react-redux';
+// import { pipelinesActions } from '../../../../../redux/actions';
 import {
   runPagesSelectors,
   runSelectors,
@@ -33,34 +33,34 @@ export const useService = ({
   sortBy: string;
   pipelineId: TId;
 }): ServiceInterface => {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
   const fetching = useSelector(runPagesSelectors.fetching);
   const runs: Run[] = useSelector(runSelectors.runsForPipelineId(pipelineId));
   const runsPaginated = useSelector(runSelectors.myRunsPaginated);
-  const isValidFilter = filter.map((f) => f.value).join('');
+  // const isValidFilter = filter.map((f) => f.value).join('');
 
   useEffect(() => {}, [runs]);
-  useEffect(() => {
-    if (!isValidFilter && !isExpended) {
-      const intervalId = setInterval(() => {
-        //assign interval to a variable to clear it.
+  // useEffect(() => {
+  //   if (!isValidFilter && !isExpended) {
+  //     const intervalId = setInterval(() => {
+  //       //assign interval to a variable to clear it.
 
-        dispatch(
-          pipelinesActions.allRunsByPipelineId({
-            sort_by: sortBy,
-            logical_operator: 'and',
-            pipelineId: pipelineId,
-            page: runsPaginated.page,
-            size: runsPaginated.size,
-          }),
-        );
-      }, 5000);
+  //       dispatch(
+  //         pipelinesActions.allRunsByPipelineId({
+  //           sort_by: sortBy,
+  //           logical_operator: 'and',
+  //           pipelineId: pipelineId,
+  //           page: runsPaginated.page,
+  //           size: runsPaginated.size,
+  //         }),
+  //       );
+  //     }, 5000);
 
-      return () => clearInterval(intervalId);
-    }
-    //This is important
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [pipelineId, runsPaginated]);
+  //     return () => clearInterval(intervalId);
+  //   }
+  //   //This is important
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [pipelineId, runsPaginated]);
 
   const runIds = runs.map((run: Run) => run.id);
 
