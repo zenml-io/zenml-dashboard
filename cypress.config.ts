07fa9802c552f81116799c899d00a51c9f79e461
talhainvenxion <talha@invenxion.com>
2023-10-09 01:05:21 +0500

    fixed

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    // baseUrl: 'http://localhost:3000',
+    baseUrl: 'https://zenml-ui-dev.netlify.app',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
