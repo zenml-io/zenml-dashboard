2daf826583ba45287282b175b08f24866bb4d546
talhainvenxion <talha@invenxion.com>
2023-10-09 02:44:55 +0500

    fixed

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
+    baseUrl: process.env.CYPRESS_BASE_URL,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
