eb21a81b51a7a8b246ba20b383d9e357d1fc6059
talhainvenxion <talha@invenxion.com>
2023-10-09 02:38:37 +0500

    fixed

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: '',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
@@ -11,8 +10,3 @@ export default defineConfig({
     numTestsKeptInMemory: 40,
   },
 });
-
-if (process.env.CYPRESS_BASE_URL) {
-  // Update the baseUrl with the environment variable value
-  module.exports.e2e.baseUrl = process.env.CYPRESS_BASE_URL;
-}
