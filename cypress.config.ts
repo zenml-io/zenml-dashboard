49dd47c9116ed76224884e4177f4dfaab24ba1bd
talhainvenxion <talha@invenxion.com>
2023-10-09 13:33:22 +0500

    fixed

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    experimentalRunAllSpecs: true,
   },
 });
