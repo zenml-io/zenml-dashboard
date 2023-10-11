79d182409820fdc15489e9bb0ed686c432c349dd
talhainvenxion <talha@invenxion.com>
2023-10-11 12:52:08 +0500

    fixed

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'cypress';
-
+require('dotenv').config();
 export default defineConfig({
+  env: { ...process.env },
   e2e: {
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
     baseUrl: 'http://localhost:3000',
