a807145d90eaffeae3bd3143cfdc840f2f45527b
talhainvenxion <talha@invenxion.com>
2023-11-24 21:30:44 +0500

    fixed

--- src/ui/components/dag/index.tsx
+++ src/ui/components/dag/index.tsx
@@ -54,7 +54,7 @@ export const DAG: React.FC<{
     return (
       <div style={styles.container}>
         <div style={styles.dag}>
-          {errorMessage.includes('IllegalOperationError') ? (
+          {errorMessage?.includes('IllegalOperationError') ? (
             <Box
               style={{
                 textAlign: 'center',
