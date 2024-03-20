837052808c7ade2836050e69b44567d948da75ca
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2024-03-20 08:32:52 +0000

    feat: initialize new app

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,35 @@
-import React from 'react';
-import { PersistGate } from 'redux-persist/integration/react';
-import { Provider } from 'react-redux';
-import { RouteConfig } from './routes';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-import configureStore from './redux/setup/storeSetup';
-import './utils/axios';
-import Toaster from './ui/layouts/common/Toaster';
+function App() {
+  const [count, setCount] = useState(0)
 
-import 'bootstrap/dist/css/bootstrap-grid.min.css';
-import './App.css';
-import './ui/default.css';
-
-const { store, persistor } = configureStore();
-if (
-  process.env.REACT_APP_DEMO_SETUP === 'true' &&
-  (process.env.REACT_APP_USERNAME === undefined ||
-    process.env.REACT_APP_PASSWORD === undefined)
-) {
-  console.warn(
-    'You need to add process.env.REACT_APP_USERNAME and process.env.REACT_APP_PASSWORD in .env file.',
-  );
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-const App: React.FC = () => {
-  return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Toaster />
-        <RouteConfig />
-      </PersistGate>
-    </Provider>
-  );
-};
-
-export default App;
+export default App
