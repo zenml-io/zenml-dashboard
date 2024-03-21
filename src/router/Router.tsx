77d963af85b6764ccd9a459f877b46189ef01ef3
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2024-03-21 14:23:25 +0000

    feat: add svgr

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,7 +1,13 @@
 import { lazy } from "react";
 import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 const Home = lazy(() => import("@/app/page"));
+const Login = lazy(() => import("@/app/login/page"));
 
 export const router = createBrowserRouter(
-	createRoutesFromElements(<Route path="/" element={<Home />}></Route>)
+	createRoutesFromElements(
+		<>
+			<Route path="/" element={<Home />} />
+			<Route path="login" element={<Login />} />
+		</>
+	)
 );
