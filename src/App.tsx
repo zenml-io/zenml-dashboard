8b3df8bb8fe335ebc3e08eb361231d5744394e4b
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2024-03-22 13:45:45 +0000

    feat: add react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { Suspense } from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/Router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export function App() {
 	return (
-		<Suspense>
-			<RouterProvider router={router} />
-		</Suspense>
+		<QueryClientProvider client={queryClient}>
+			<Suspense>
+				<RouterProvider router={router} />
+			</Suspense>
+		</QueryClientProvider>
 	);
 }
