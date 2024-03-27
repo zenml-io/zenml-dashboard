f86eab909597d61fdcc122d9d8b959d941f44461
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2024-03-27 11:00:43 +0000

    feat: add sidebar

--- src/layouts/AuthenticatedLayout/index.tsx
+++ src/layouts/AuthenticatedLayout/index.tsx
@@ -1,12 +1,19 @@
 import { Outlet } from "react-router-dom";
 import { AuthenticatedHeader } from "./AuthenticatedHeader";
+import { Sidebar } from "./Sidebar";
 
 export function AuthenticatedLayout() {
 	return (
 		<div className="relative flex min-h-screen w-full flex-col">
 			<AuthenticatedHeader />
 			<main className="flex flex-grow flex-col">
-				<Outlet />
+				<div className="flex flex-grow">
+					<Sidebar />
+
+					<div className="w-full">
+						<Outlet />
+					</div>
+				</div>
 			</main>
 		</div>
 	);
