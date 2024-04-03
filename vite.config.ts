55d669c86dd2760c619d09db5c42d96be032c6b5
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2024-04-03 10:53:42 +0000

    chore: prefetch bundle

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
+import prefetchPlugin from "vite-plugin-bundle-prefetch";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [svgr(), react()],
+	plugins: [svgr(), react(), prefetchPlugin()],
 	resolve: {
 		alias: {
 			"@": "/src"
