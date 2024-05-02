9685474037d63d39b113417a3b29d0c73e49ed20
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2024-05-02 13:22:32 +0200

    docs: Update Readme (#583)
    Co-authored-by: Hamza Tahir <htahir111@gmail.com>
    
    ---------
    
    Co-authored-by: Hamza Tahir <htahir111@gmail.com>

--- src/components/password/UpdatePasswordSchemas.ts
+++ src/components/password/UpdatePasswordSchemas.ts
@@ -6,7 +6,7 @@ export const passwordSchema = z
 	.regex(/[A-Z]/, "Password must contain at least one uppercase letter")
 	.regex(/[a-z]/, "Password must contain at least one lowercase letter")
 	.regex(/[0-9]/, "Password must contain at least one number")
-	.regex(/[!@#$%^&*(),.?":{}|<>]/, "Password must contain at least one special character");
+	.regex(/[!@#$%^&*(),.?":{}|<>\/]/, "Password must contain at least one special character");
 
 export const updatePasswordBaseFormSchema = z.object({
 	oldPassword: z.string().optional(),
