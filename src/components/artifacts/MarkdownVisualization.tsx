d8aafc6b1dfed39f539d5ab14f18ed328f772a37
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2024-05-07 12:11:41 +0000

    feat: make prose important

--- src/components/artifacts/MarkdownVisualization.tsx
+++ src/components/artifacts/MarkdownVisualization.tsx
@@ -2,5 +2,5 @@ import { Props } from "./Visualization";
 import Markdown from "react-markdown";
 
 export default function MarkdownVisualization({ content }: Props) {
-	return <Markdown className="prose max-w-none">{content}</Markdown>;
+	return <Markdown className="!prose !max-w-none">{content}</Markdown>;
 }
