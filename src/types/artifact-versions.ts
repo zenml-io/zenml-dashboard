72800865fe8b44067688d9d8857d08d536113df4
Atzin Escandia <atzin.ext@zenml.io>
2024-05-27 14:05:13 +0200

    code repo fixes - clean

--- src/types/artifact-versions.ts
+++ src/types/artifact-versions.ts
@@ -1,13 +1,6 @@
-import { components, operations } from "./core";
+import { components } from "./core";
 
 export type ArtifactVersionBody = components["schemas"]["ArtifactVersionResponseBody"];
 export type ArtifactVersion = components["schemas"]["ArtifactVersionResponse"];
 // Artifact Visualization
 export type ArtifactVisualization = components["schemas"]["LoadedVisualization"];
-
-export type CodeRepository = components["schemas"]["CodeRepositoryResponse"];
-export type PageCodeRepositoryResponse = components["schemas"]["Page_CodeRepositoryResponse_"];
-
-export type CodeRepositoryListQueryParams = NonNullable<
-	operations["list_code_repositories_api_v1_code_repositories_get"]["parameters"]["query"]
->;
