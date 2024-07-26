8e31c1ab9f091d4ec5ec05afb26ac5648f1470c2
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2024-07-26 09:13:38 +0000

    test: add test

--- src/app/stacks/create/existing-infrastructure/steps/artifact_store/Header.tsx
+++ src/app/stacks/create/existing-infrastructure/steps/artifact_store/Header.tsx
@@ -1,10 +1,10 @@
-import Folder from "@/assets/icons/folder.svg?react";
+import { ComponentIcon } from "@/components/ComponentIcon";
 
 export function ArtifactStoreHeader() {
 	return (
 		<div className="space-y-1">
 			<p className="flex items-center gap-1 text-text-lg font-semibold">
-				<Folder className="h-5 w-5 fill-primary-400" />
+				<ComponentIcon type="artifact_store" className="h-5 w-5 fill-primary-400" />
 				Select your Artifact Store
 			</p>
 			<p className="text-theme-text-secondary">
