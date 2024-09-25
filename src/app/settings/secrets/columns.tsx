814d692661d29921cc7550dc0bf0f37199d1c660
Aagya-Sharma <sharmaaagya7@gmail.com>
2024-09-25 06:58:36 +0545

    feat: add secret tooltip

--- src/app/settings/secrets/columns.tsx
+++ src/app/settings/secrets/columns.tsx
@@ -8,6 +8,14 @@ import { User } from "@/types/user";
 import { ColumnDef } from "@tanstack/react-table";
 import { NavigateFunction } from "react-router-dom";
 import SecretsDropdown from "./SecretsDropdown";
+import Info from "@/assets/icons/info.svg?react";
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger
+} from "@zenml-io/react-component-library";
+import { Codesnippet } from "@/components/CodeSnippet";
 
 export function getSecretColumns(navigate: NavigateFunction): ColumnDef<SecretNamespace>[] {
 	return [
@@ -20,15 +28,52 @@ export function getSecretColumns(navigate: NavigateFunction): ColumnDef<SecretNa
 					<LockIcon className="h-4 w-4 flex-shrink-0 cursor-pointer fill-primary-400" />
 					<div className="flex flex-col">
 						<div className="group/copybutton flex flex-row items-center gap-0.5 space-x-1">
-							<span
-								className=" text-text-md font-semibold text-theme-text-primary"
-								onClick={() => navigate(`/settings/secrets/${row.original.id}`)}
-							>
-								{getValue<string>()}
-							</span>
+							{/* Flex container to ensure name and icon are aligned horizontally */}
+							<div className="flex flex-row items-center">
+								<span
+									className="text-text-md font-semibold text-theme-text-primary"
+									onClick={() => navigate(`/settings/secrets/${row.original.id}`)}
+								>
+									{getValue<string>()}
+								</span>
+								{/* Icon placed next to the name */}
+								<TooltipProvider>
+									<Tooltip>
+										<TooltipTrigger asChild>
+											<div className="">
+												<Info className="ml-1 h-4 w-4 shrink-0 fill-theme-text-tertiary" />
+											</div>
+										</TooltipTrigger>
+										<TooltipContent
+											align="start"
+											side="bottom"
+											className=" z-50 flex max-w-[480px] flex-col gap-2 bg-theme-surface-primary p-5 text-text-sm text-theme-text-primary"
+										>
+											<p>To use your secret in a step, you can use the following code:</p>
+											<Codesnippet
+												fullWidth
+												highlightCode
+												wrap
+												codeClasses="whitespace-pre-wrap break-words" // Use break-words for more natural wrapping
+												code={`from zenml.client import Client
+secret = Client().get_secret(${getValue<string>()})
+`}
+											/>
+
+											<a
+												className="link text-primary-400"
+												target="_blank"
+												href="https://docs.zenml.io/how-to/interact-with-secrets#accessing-registered-secrets"
+											>
+												Learn More
+											</a>
+										</TooltipContent>
+									</Tooltip>
+								</TooltipProvider>
+							</div>
 							<CopyButton copyText={getValue<string>()} />
 						</div>
-						<span className="text-sm text-gray-500"> {row.original.id.slice(0, 8)}</span>
+						<span className="text-sm text-gray-500">{row.original.id.slice(0, 8)}</span>
 					</div>
 				</div>
 			)
--- src/app/settings/secrets/secretsDetail/columns.tsx
+++ src/app/settings/secrets/secretsDetail/columns.tsx
@@ -3,6 +3,14 @@ import { ColumnDef } from "@tanstack/react-table";
 import KeyIcon from "@/assets/icons/key-icon.svg?react";
 import EyeIcon from "@/assets/icons/eye.svg?react";
 import SecretTableDropDown from "./SecretTableDropDown";
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger
+} from "@zenml-io/react-component-library";
+import { Codesnippet } from "@/components/CodeSnippet";
+import Info from "@/assets/icons/info.svg?react";
 
 const ValueCell: React.FC<{ value: unknown }> = ({ value }) => {
 	const [isVisible, setIsVisible] = useState(false);
@@ -25,20 +33,67 @@ export function getSecretDetailColumn(secretId: any): ColumnDef<any>[] {
 		{
 			id: "key",
 			header: "Key",
-			accessorKey: "key",
-			cell: ({ row }) => (
-				<div className="flex items-center space-x-2">
-					<KeyIcon className="h-4 w-4 flex-shrink-0 cursor-pointer" />
-					<div className="flex flex-col">
-						<div className="flex flex-row space-x-1">
-							<span className="text-text-md font-semibold text-theme-text-primary">
-								{row.getValue("key")}
-							</span>
+			accessorFn: (row) => ({
+				key: row.key,
+				name: row.name
+			}),
+			cell: ({ getValue }) => {
+				const { name, key } = getValue<{
+					key: string;
+					name: string;
+				}>();
+				return (
+					<div className="flex items-center space-x-2">
+						<KeyIcon className="h-4 w-4 flex-shrink-0" />
+						<div className="flex flex-col">
+							<div className="flex flex-row items-center gap-0.5 space-x-1">
+								{/* Flex container to ensure name and icon are aligned horizontally */}
+								<div className="flex flex-row items-center">
+									<span className="text-text-md font-semibold text-theme-text-primary">{key}</span>
+
+									{/* Icon placed next to the name */}
+									<TooltipProvider>
+										<Tooltip>
+											<TooltipTrigger asChild>
+												<div className="">
+													<Info className="ml-1 h-4 w-4 shrink-0 cursor-pointer fill-theme-text-tertiary" />
+												</div>
+											</TooltipTrigger>
+											<TooltipContent
+												align="start"
+												side="bottom"
+												className="z-50 flex max-w-[480px] flex-col gap-2 bg-theme-surface-primary p-5 text-text-sm text-theme-text-primary"
+											>
+												<p>To use your secret in a step, you can use the following code:</p>
+												<Codesnippet
+													highlightCode
+													wrap
+													codeClasses="whitespace-pre-wrap break-words"
+													code={`from zenml.client import Client
+secret = Client().get_secret(${name})
+	  
+# 'secret.secret_values' will contain a dictionary with all key-value pairs within your secret.
+secret.secret_values["key"]
+	  `}
+												/>
+												<a
+													className="link text-primary-400"
+													target="_blank"
+													href="https://docs.zenml.io/how-to/interact-with-secrets#accessing-registered-secrets"
+												>
+													Learn More
+												</a>
+											</TooltipContent>
+										</Tooltip>
+									</TooltipProvider>
+								</div>
+							</div>
 						</div>
 					</div>
-				</div>
-			)
+				);
+			}
 		},
+
 		{
 			id: "value",
 			header: "Value",
