44aee75b63c38527f4fe8013f085437f58ca1af4
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2024-11-06 09:07:23 +0000

    feat: adjust breadcrumb

--- src/app/upgrade/page.tsx
+++ src/app/upgrade/page.tsx
@@ -1,7 +1,13 @@
+import { useEffect } from "react";
+import { useBreadcrumbsContext } from "../../layouts/AuthenticatedLayout/BreadcrumbsContext";
 import { UpgradeProvider } from "./components/Context";
 import { UpgradeWrapperBox } from "./components/form/Wrapper";
 
 export default function UpgradePage() {
+	const { setCurrentBreadcrumbData } = useBreadcrumbsContext();
+	useEffect(() => {
+		setCurrentBreadcrumbData({ segment: "upgrade", data: null });
+	}, []);
 	return (
 		<div className="flex h-full items-center justify-center">
 			<div className="layout-container">
