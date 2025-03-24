ce1a422ad6921c31d8f240aef990622bbfd5375f
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2025-03-24 10:56:29 +0000

    chore: update header

--- src/app/models/page.tsx
+++ src/app/models/page.tsx
@@ -19,7 +19,7 @@ export default function ModelsPage() {
 	return (
 		<div>
 			<PageHeader className="flex items-center gap-1">
-				<h1 className="text-display-xs font-semibold">Artifacts</h1>
+				<h1 className="text-display-xs font-semibold">Models</h1>
 			</PageHeader>
 			<div className="layout-container flex flex-col items-center space-y-7 py-5">
 				<UpgradeFallback />
