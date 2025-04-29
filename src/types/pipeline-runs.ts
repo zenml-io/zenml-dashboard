5d0b270d2b80fa567750ab7fe184c297eaca37c7
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2025-04-29 08:29:40 +0000

    feat: updates for client env

--- src/types/pipeline-runs.ts
+++ src/types/pipeline-runs.ts
@@ -9,6 +9,13 @@ export type PipelineRunBody = components["schemas"]["PipelineRunResponseBody"];
 export type PipelineRunOvervieweParams = NonNullable<
 	operations["list_runs_api_v1_runs_get"]["parameters"]["query"]
 >;
+
+export type PipelineRunDetailQueryParams = NonNullable<
+	operations["get_run_api_v1_runs__run_id__get"]["parameters"]["query"]
+> & {
+	// remove this union after the new types are generated
+	include_python_packages?: boolean;
+};
 export type PipelineRunPage = components["schemas"]["Page_PipelineRunResponse_"];
 
 type ArtifactNodeDetails = ArtifactVersion & { name: string; artifactType: "input" | "output" };
