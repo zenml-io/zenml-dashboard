97ec672a0265d5e12c47e7eaee26d0f2d3fec0ef
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2025-05-15 06:47:50 +0000

    feat: add whats new button

--- src/layouts/AuthenticatedLayout/UserDropdown.tsx
+++ src/layouts/AuthenticatedLayout/UserDropdown.tsx
@@ -1,4 +1,6 @@
+import File from "@/assets/icons/file.svg?react";
 import LogoutIcon from "@/assets/icons/logout.svg?react";
+import Users from "@/assets/icons/users.svg?react";
 import { useAuthContext } from "@/context/AuthContext";
 import { useServerInfo } from "@/data/server/info-query";
 import { useLogoutMutation } from "@/data/session/logout-mutation";
@@ -14,6 +16,7 @@ import {
 	Skeleton
 } from "@zenml-io/react-component-library";
 import { useNavigate } from "react-router-dom";
+import ChangeLogButton from "./changelog-button";
 
 export function UserDropdown() {
 	const currentUser = useCurrentUser();
@@ -39,7 +42,30 @@ export function UserDropdown() {
 				</Avatar>
 			</DropdownMenuTrigger>
 			<DropdownMenuContent align="end" sideOffset={7}>
+				<ChangeLogButton />
+				<DropdownMenuItem asChild icon={<File />}>
+					<a
+						className="cursor-pointer"
+						rel="noopener noreferrer"
+						target="_blank"
+						href="https://docs.zenml.io/"
+					>
+						Docs
+					</a>
+				</DropdownMenuItem>
+				<DropdownMenuItem asChild icon={<Users />}>
+					<a
+						className="cursor-pointer"
+						rel="noopener noreferrer"
+						target="_blank"
+						href="https://zenml.io/slack"
+					>
+						Community
+					</a>
+				</DropdownMenuItem>
+				<div aria-hidden="true" className="my-1 h-[1px] bg-theme-border-moderate"></div>
 				<DropdownMenuItem
+					className="cursor-pointer"
 					disabled={isNoAuthServer(serverInfo.data.auth_scheme)}
 					onClick={() => mutation.mutate()}
 					icon={<LogoutIcon className="h-3 w-3 fill-neutral-400" />}
