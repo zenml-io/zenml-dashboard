7f203411936d32e0c3f4dd04363e41be7293e030
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2025-06-12 08:26:24 +0000

    feat: add tooltip

---
+++ src/app/settings/api-tokens/DisabledTokenButton.tsx
@@ -0,0 +1,31 @@
+import {
+	cn,
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger
+} from "@zenml-io/react-component-library";
+import { ButtonProps, buttonVariants } from "@zenml-io/react-component-library/components/server";
+
+type Props = {
+	size: ButtonProps["size"];
+};
+
+export function DisabledTokenButton({ size }: Props) {
+	return (
+		<div className="flex flex-wrap items-center justify-end gap-2">
+			<TooltipProvider>
+				<Tooltip>
+					<TooltipTrigger className="z-10">
+						<div className={cn(buttonVariants({ size }), "pointer-events-none bg-primary-50")}>
+							Create new token
+						</div>
+					</TooltipTrigger>
+					<TooltipContent>
+						Api Tokens are not available in the current deployment mode.
+					</TooltipContent>
+				</Tooltip>
+			</TooltipProvider>
+		</div>
+	);
+}
