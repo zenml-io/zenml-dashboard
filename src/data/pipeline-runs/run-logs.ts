7540dce3b9de2995470c5092f98d3526e727fff3
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2025-07-08 06:56:02 +0000

    feat: support multiple sources

--- src/data/pipeline-runs/run-logs.ts
+++ src/data/pipeline-runs/run-logs.ts
@@ -3,17 +3,21 @@ import { notFound } from "@/lib/not-found-error";
 import { UseQueryOptions, useQuery } from "@tanstack/react-query";
 import { apiPaths, createApiPath } from "../api";
 import { fetcher } from "../fetch";
+import { RunLogsQueryParams } from "@/types/logs";
+import { objectToSearchParams } from "@/lib/url";
 
 export type PipelineRunDetailOverview = {
 	runId: string;
+	queries: RunLogsQueryParams;
 };
 
-export function getRunLogsQueryKey({ runId }: PipelineRunDetailOverview) {
-	return ["runs", runId, "logs"];
+export function getRunLogsQueryKey({ runId, queries }: PipelineRunDetailOverview) {
+	return ["runs", runId, "logs", queries];
 }
 
-export async function fetchRunLogs({ runId }: PipelineRunDetailOverview): Promise<string> {
-	const url = createApiPath(apiPaths.runs.logs(runId));
+export async function fetchRunLogs({ runId, queries }: PipelineRunDetailOverview): Promise<string> {
+	const queryString = objectToSearchParams(queries).toString();
+	const url = createApiPath(apiPaths.runs.logs(runId)) + (queryString ? `?${queryString}` : "");
 	const res = await fetcher(url, {
 		method: "GET",
 		headers: {
