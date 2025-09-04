3428afac823f900426a3f8f8f1557c00d3180a6d
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2025-09-04 12:25:48 +0200

    feat: tooltips

--- src/app/runs/[id]/pipeline-viz/view-switcher.tsx
+++ src/app/runs/[id]/pipeline-viz/view-switcher.tsx
@@ -1,7 +1,13 @@
 import Clock from "@/assets/icons/clock.svg?react";
 import Dataflow from "@/assets/icons/dataflow.svg?react";
 import { Tabs, TabsList, TabsTrigger } from "@radix-ui/react-tabs";
-import { Button } from "@zenml-io/react-component-library/components";
+import {
+	Button,
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger
+} from "@zenml-io/react-component-library/components";
 import { ComponentPropsWithoutRef, ElementRef, forwardRef } from "react";
 import { PiplineRunVisualizationView } from "./types";
 
@@ -12,34 +18,46 @@ type Props = {
 
 export function ViewSwitcher({ activeView, setActiveView }: Props) {
 	return (
-		<Tabs
-			className="w-fit overflow-hidden rounded-md border border-theme-border-moderate bg-theme-surface-primary"
-			value={activeView}
-			onValueChange={(value) => setActiveView(value as PiplineRunVisualizationView)}
-		>
-			<TabsList className="flex items-center">
-				<TabsTrigger asChild value="dag">
-					<ButtonTrigger
-						data-state-active={activeView === "dag"}
-						view="dag"
-						activeView={activeView}
-					>
-						<Dataflow className="size-4 group-data-[state=active]:fill-theme-text-negative" />
-						<span className="sr-only">DAG</span>
-					</ButtonTrigger>
-				</TabsTrigger>
-				<TabsTrigger asChild value="timeline">
-					<ButtonTrigger
-						data-state-active={activeView === "timeline"}
-						view="timeline"
-						activeView={activeView}
-					>
-						<Clock className="size-4 group-data-[state=active]:fill-theme-text-negative" />
-						<span className="sr-only">Timeline</span>
-					</ButtonTrigger>
-				</TabsTrigger>
-			</TabsList>
-		</Tabs>
+		<TooltipProvider>
+			<Tabs
+				className="w-fit overflow-hidden rounded-md border border-theme-border-moderate bg-theme-surface-primary"
+				value={activeView}
+				onValueChange={(value) => setActiveView(value as PiplineRunVisualizationView)}
+			>
+				<TabsList className="flex items-center">
+					<Tooltip>
+						<TabsTrigger asChild value="dag">
+							<TooltipTrigger asChild>
+								<ButtonTrigger
+									data-state-active={activeView === "dag"}
+									view="dag"
+									activeView={activeView}
+								>
+									<Dataflow className="size-4 group-data-[state=active]:fill-theme-text-negative" />
+									<span className="sr-only">DAG</span>
+								</ButtonTrigger>
+							</TooltipTrigger>
+						</TabsTrigger>
+						<TooltipContent>DAG View</TooltipContent>
+					</Tooltip>
+					<Tooltip>
+						<TabsTrigger asChild value="timeline">
+							<TooltipTrigger asChild>
+								<ButtonTrigger
+									data-state-active={activeView === "timeline"}
+									view="timeline"
+									activeView={activeView}
+								>
+									<Clock className="size-4 group-data-[state=active]:fill-theme-text-negative" />
+									<span className="sr-only">Timeline</span>
+								</ButtonTrigger>
+							</TooltipTrigger>
+						</TabsTrigger>
+						<TooltipContent>Timeline View</TooltipContent>
+					</Tooltip>
+				</TabsList>
+			</Tabs>
+		</TooltipProvider>
 	);
 }
 
