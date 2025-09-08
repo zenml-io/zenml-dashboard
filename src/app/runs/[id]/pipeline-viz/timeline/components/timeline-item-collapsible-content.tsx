f38ec81b69cf433fc4bcf1dbe3de5525afe5c0b0
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2025-09-08 11:30:22 +0200

    refactor: arrows

--- src/app/runs/[id]/pipeline-viz/timeline/components/timeline-item-collapsible-content.tsx
+++ src/app/runs/[id]/pipeline-viz/timeline/components/timeline-item-collapsible-content.tsx
@@ -15,41 +15,47 @@ export function TimelineItemCollapsibleContent({ timelineItem, runStatus }: Prop
 	const inputs = timelineItem.inputs;
 	const outputs = timelineItem.outputs;
 	return (
-		<div className="flex items-center gap-3 overflow-x-auto px-5 py-2">
-			{inputs.length > 0 && (
-				<>
-					<div className="space-y-1">
-						<Headline>Input Artifacts ({inputs.length})</Headline>
-						<ul className="flex items-center gap-1">
-							{inputs.map((input) => (
-								<li key={input.node_id}>
-									<TimelineArtifactNode artifact={input} />
-								</li>
-							))}
-						</ul>
-					</div>
-					<LongArrowRight className="w-[30px] shrink-0 fill-neutral-400" />
-				</>
-			)}
-			<div className="space-y-1">
-				<Headline>Step</Headline>
-				<TimelineStepNode step={step} runStatus={runStatus} />
+		<div className="overflow-x-auto px-5 py-2">
+			<div className="flex items-end gap-2">
+				{inputs.length > 0 && (
+					<>
+						<div className="space-y-1">
+							<Headline>Input Artifacts ({inputs.length})</Headline>
+							<ul className="flex items-center gap-1">
+								{inputs.map((input) => (
+									<li key={input.node_id}>
+										<TimelineArtifactNode artifact={input} />
+									</li>
+								))}
+							</ul>
+						</div>
+						<div className="flex h-6 items-center">
+							<LongArrowRight className="w-[30px] shrink-0 fill-neutral-400" />
+						</div>
+					</>
+				)}
+				<div className="space-y-1">
+					<Headline>Step</Headline>
+					<TimelineStepNode step={step} runStatus={runStatus} />
+				</div>
+				{outputs.length > 0 && (
+					<>
+						<div className="flex h-6 items-center">
+							<LongArrowRight className="w-[30px] shrink-0 fill-neutral-400" />
+						</div>
+						<div className="space-y-1">
+							<Headline>Output Artifacts ({outputs.length})</Headline>
+							<ul className="flex items-center gap-1">
+								{outputs.map((output) => (
+									<li key={output.node_id}>
+										<TimelineArtifactNode artifact={output} />
+									</li>
+								))}
+							</ul>
+						</div>
+					</>
+				)}
 			</div>
-			{outputs.length > 0 && (
-				<>
-					<LongArrowRight className="w-[30px] shrink-0 fill-neutral-400" />
-					<div className="space-y-1">
-						<Headline>Output Artifacts ({outputs.length})</Headline>
-						<ul className="flex items-center gap-1">
-							{outputs.map((output) => (
-								<li key={output.node_id}>
-									<TimelineArtifactNode artifact={output} />
-								</li>
-							))}
-						</ul>
-					</div>
-				</>
-			)}
 		</div>
 	);
 }
