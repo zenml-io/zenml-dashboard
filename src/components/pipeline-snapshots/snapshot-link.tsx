62a619ad03181bd612576dab7029ac0fdcfef090
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2025-09-26 10:50:18 +0200

    feat: add icon

--- src/components/pipeline-snapshots/snapshot-link.tsx
+++ src/components/pipeline-snapshots/snapshot-link.tsx
@@ -1,3 +1,4 @@
+import SnapshotIcon from "@/assets/icons/snapshot.svg?react";
 import { routes } from "@/router/routes";
 import { Tag } from "@zenml-io/react-component-library";
 import { ComponentProps } from "react";
@@ -8,7 +9,10 @@ type Props = ComponentProps<typeof Tag> & {
 	snapshotName: string;
 };
 
-export function SnapshotLink({ snapshotId, snapshotName, ...props }: Props) {
+export function SnapshotLink({ snapshotId, snapshotName, size, ...props }: Props) {
+	const iconSize = size || "sm";
+	const iconClassName = iconSize === "sm" ? "size-4" : "size-3";
+
 	return (
 		<Link to={routes.projects.snapshots.detail.overview(snapshotId)}>
 			<Tag
@@ -16,9 +20,11 @@ export function SnapshotLink({ snapshotId, snapshotName, ...props }: Props) {
 				className="inline-flex items-center gap-0.5 text-theme-text-primary"
 				rounded={false}
 				emphasis="subtle"
+				size={size}
 				{...props}
 			>
-				{snapshotName}
+				<SnapshotIcon className={`${iconClassName} shrink-0 fill-theme-text-tertiary`} />
+				<span>{snapshotName}</span>
 			</Tag>
 		</Link>
 	);
