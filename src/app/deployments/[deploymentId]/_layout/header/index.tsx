f75d1f6ce00c6f51aad3173b3033f681f210b3ca
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2025-10-22 12:09:09 +0200

    fix: playground

--- src/app/deployments/[deploymentId]/_layout/header/index.tsx
+++ src/app/deployments/[deploymentId]/_layout/header/index.tsx
@@ -3,7 +3,7 @@ import { DeploymentDetailTabs } from "./tabs";
 
 export function DeploymentDetailHeader({ deploymentId }: { deploymentId: string }) {
 	return (
-		<section className="overflow-x-hidden border-b border-theme-border-moderate bg-theme-surface-primary">
+		<section className="shrink-0 overflow-x-hidden border-b border-theme-border-moderate bg-theme-surface-primary">
 			<div className="space-y-1 px-5 pt-5 lg:px-[80px]">
 				<DeploymentDetailHeaderInfo deploymentId={deploymentId} />
 				<DeploymentDetailTabs />
