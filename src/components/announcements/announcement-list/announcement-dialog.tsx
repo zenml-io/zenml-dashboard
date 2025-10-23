6e6e327173b741ee99af1fe871128f010084e420
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2025-10-23 08:47:17 +0200

    feat: add empty state

--- src/components/announcements/announcement-list/announcement-dialog.tsx
+++ src/components/announcements/announcement-list/announcement-dialog.tsx
@@ -2,6 +2,7 @@ import Items from "@/contents/whats-new.json";
 import { Dialog, DialogContent, DialogDescription } from "@zenml-io/react-component-library";
 import { AnnouncementDialogHeader } from "./announcement-dialog-header";
 import { AnnouncementItem } from "./announcement-item";
+import { AnnouncementEmptyState } from "./announcement-empty-state";
 
 type Props = {
 	open: boolean;
@@ -11,18 +12,24 @@ type Props = {
 export function AnnouncementDialog({ open, setOpen }: Props) {
 	const publishedItems = Items.entries.filter((item) => item.published);
 
+	const isEmpty = publishedItems.length <= 1;
+
 	return (
 		<Dialog open={open} onOpenChange={setOpen}>
 			<DialogContent className="flex max-h-[600px] max-w-[600px] flex-col">
 				<AnnouncementDialogHeader />
 				<DialogDescription className="sr-only">Whats' new in ZenML</DialogDescription>
-				<ul className="space-y-8 overflow-y-auto p-5">
-					{publishedItems.map((item) => (
-						<li key={item.id}>
-							<AnnouncementItem key={item.id} item={item} />
-						</li>
-					))}
-				</ul>
+				{isEmpty ? (
+					<AnnouncementEmptyState />
+				) : (
+					<ul className="space-y-8 overflow-y-auto p-5">
+						{publishedItems.map((item) => (
+							<li key={item.id}>
+								<AnnouncementItem key={item.id} item={item} />
+							</li>
+						))}
+					</ul>
+				)}
 			</DialogContent>
 		</Dialog>
 	);
