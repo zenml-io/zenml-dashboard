20bc0cda8dccc2540a1e515036dcca5651701ff3
Tim Zadorozhny <tim@zenml.io>
2025-10-31 16:10:37 +0100

    back to 255 limit

--- src/app/stacks/create/components/sharedSchema.ts
+++ src/app/stacks/create/components/sharedSchema.ts
@@ -5,9 +5,9 @@ export const stackNameSchema = z
 	.string()
 	.trim()
 	.min(1, "Stack name is required")
-	.max(32, "Stack name must be less than 32 characters")
-	.refine((name) => validateStackNameNotInUse(name), "Stack name is already in use")
+	.max(255, "Stack name must be less than 255 characters")
 	.refine(
 		(name) => name.match(/^[a-zA-Z0-9-]+$/),
 		"Stack name can only contain only letters, numbers and dashes"
-	);
+	)
+	.refine((name) => validateStackNameNotInUse(name), "Stack name is already in use");
