09456f0039b683c8ff615ca65840861e00d10d6e
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2025-11-05 15:38:13 +0100

    fix: error handling

--- src/components/announcements/announcement-list/announcement-dialog.tsx
+++ src/components/announcements/announcement-list/announcement-dialog.tsx
@@ -1,8 +1,9 @@
 import { useAnnouncements } from "@/data/announcements/announcements";
 import { Dialog, DialogContent, DialogDescription } from "@zenml-io/react-component-library";
+import { useMemo } from "react";
 import { AnnouncementDialogHeader } from "./announcement-dialog-header";
 import { AnnouncementDialogSearchBar } from "./announcement-dialog-search-bar";
-import { AnnouncementEmptyState } from "./announcement-empty-state";
+import { AnnouncementErrorState, NoAnnouncementEmptyState } from "./announcement-empty-state";
 import { AnnouncementItem } from "./announcement-item";
 import { useAnnouncementSearch } from "./use-announcement-search";
 
@@ -14,36 +15,47 @@ type Props = {
 export function AnnouncementDialog({ open, setOpen }: Props) {
 	const announcementsQuery = useAnnouncements();
 
-	const publishedItems = announcementsQuery.isSuccess
-		? announcementsQuery.data.filter((item) => item.published)
-		: [];
+	const publishedItems = useMemo(
+		() =>
+			announcementsQuery.isSuccess ? announcementsQuery.data.filter((item) => item.published) : [],
+		[announcementsQuery.isSuccess, announcementsQuery.data]
+	);
 
 	const { searchOpen, toggleSearch, searchQuery, setSearchQuery, filteredAnnouncements } =
 		useAnnouncementSearch(publishedItems);
 
 	if (announcementsQuery.isPending) return null;
-	if (announcementsQuery.isError) return null;
 
 	const isEmpty = publishedItems.length <= 1;
+	const showEmptyState = isEmpty || filteredAnnouncements.length === 0;
 
 	return (
 		<Dialog open={open} onOpenChange={setOpen}>
 			<DialogContent className="flex max-h-[600px] max-w-[600px] flex-col">
 				<AnnouncementDialogHeader searchOpen={searchOpen} toggleSearch={toggleSearch} />
-				<DialogDescription className="sr-only">Whats' new in ZenML</DialogDescription>
-				{searchOpen && (
-					<AnnouncementDialogSearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
-				)}
-				{isEmpty || filteredAnnouncements.length === 0 ? (
-					<AnnouncementEmptyState />
+				<DialogDescription className="sr-only">What's new in ZenML</DialogDescription>
+				{announcementsQuery.isError ? (
+					<AnnouncementErrorState />
 				) : (
-					<ul className="space-y-8 overflow-y-auto p-5">
-						{filteredAnnouncements.map((item) => (
-							<li key={item.id}>
-								<AnnouncementItem key={item.id} item={item} />
-							</li>
-						))}
-					</ul>
+					<>
+						{searchOpen && (
+							<AnnouncementDialogSearchBar
+								searchQuery={searchQuery}
+								setSearchQuery={setSearchQuery}
+							/>
+						)}
+						{showEmptyState ? (
+							<NoAnnouncementEmptyState />
+						) : (
+							<ul className="space-y-8 overflow-y-auto p-5">
+								{filteredAnnouncements.map((item) => (
+									<li key={item.id}>
+										<AnnouncementItem item={item} />
+									</li>
+								))}
+							</ul>
+						)}
+					</>
 				)}
 			</DialogContent>
 		</Dialog>
--- src/components/announcements/announcement-list/announcement-empty-state.tsx
+++ src/components/announcements/announcement-list/announcement-empty-state.tsx
@@ -1,15 +1,38 @@
 import Speaker from "@/assets/icons/announcement.svg?react";
 
-export function AnnouncementEmptyState() {
+type Props = {
+	title: string;
+	subtitle: string;
+};
+
+function AnnouncementEmptyState({ title, subtitle }: Props) {
 	return (
 		<div className="p-5">
 			<div className="flex flex-col items-center justify-center space-y-5 rounded-md border border-dashed border-theme-border-moderate p-5">
 				<Speaker className="size-10 shrink-0 fill-neutral-300" />
 				<div className="space-y-0.5 text-center">
-					<p className="text-text-lg font-semibold text-theme-text-primary">No Announcements</p>
-					<p className="text-theme-text-secondary">There are no announcements to display.</p>
+					<p className="text-text-lg font-semibold text-theme-text-primary">{title}</p>
+					<p className="text-theme-text-secondary">{subtitle}</p>
 				</div>
 			</div>
 		</div>
+	);
+}
+
+export function NoAnnouncementEmptyState() {
+	return (
+		<AnnouncementEmptyState
+			title="No Announcements"
+			subtitle="There are no announcements to display."
+		/>
+	);
+}
+
+export function AnnouncementErrorState() {
+	return (
+		<AnnouncementEmptyState
+			title="Error loading announcements"
+			subtitle="Please try again later."
+		/>
 	);
 }
