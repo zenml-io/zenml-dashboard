8dd7dd0e6bfd65fd318b519463a55a9343165bf4
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2025-11-05 09:41:51 +0100

    feat: search

--- src/components/announcements/announcement-list/announcement-dialog.tsx
+++ src/components/announcements/announcement-list/announcement-dialog.tsx
@@ -1,8 +1,10 @@
+import { useAnnouncements } from "@/data/announcements/announcements";
 import { Dialog, DialogContent, DialogDescription } from "@zenml-io/react-component-library";
 import { AnnouncementDialogHeader } from "./announcement-dialog-header";
-import { AnnouncementItem } from "./announcement-item";
+import { AnnouncementDialogSearchBar } from "./announcement-dialog-search-bar";
 import { AnnouncementEmptyState } from "./announcement-empty-state";
-import { useAnnouncements } from "@/data/announcements/announcements";
+import { AnnouncementItem } from "./announcement-item";
+import { useAnnouncementSearch } from "./use-announcement-search";
 
 type Props = {
 	open: boolean;
@@ -11,24 +13,32 @@ type Props = {
 
 export function AnnouncementDialog({ open, setOpen }: Props) {
 	const announcementsQuery = useAnnouncements();
+
+	const publishedItems = announcementsQuery.isSuccess
+		? announcementsQuery.data.filter((item) => item.published)
+		: [];
+
+	const { searchOpen, toggleSearch, searchQuery, setSearchQuery, filteredAnnouncements } =
+		useAnnouncementSearch(publishedItems);
+
 	if (announcementsQuery.isPending) return null;
 	if (announcementsQuery.isError) return null;
-
-	const announcements = announcementsQuery.data;
-	const publishedItems = announcements.filter((item) => item.published);
 
 	const isEmpty = publishedItems.length <= 1;
 
 	return (
 		<Dialog open={open} onOpenChange={setOpen}>
 			<DialogContent className="flex max-h-[600px] max-w-[600px] flex-col">
-				<AnnouncementDialogHeader />
+				<AnnouncementDialogHeader searchOpen={searchOpen} toggleSearch={toggleSearch} />
 				<DialogDescription className="sr-only">Whats' new in ZenML</DialogDescription>
-				{isEmpty ? (
+				{searchOpen && (
+					<AnnouncementDialogSearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+				)}
+				{isEmpty || filteredAnnouncements.length === 0 ? (
 					<AnnouncementEmptyState />
 				) : (
 					<ul className="space-y-8 overflow-y-auto p-5">
-						{publishedItems.map((item) => (
+						{filteredAnnouncements.map((item) => (
 							<li key={item.id}>
 								<AnnouncementItem key={item.id} item={item} />
 							</li>
---
+++ src/components/announcements/announcement-list/get-filtered-items.ts
@@ -0,0 +1,15 @@
+import { Announcement } from "@/data/announcements/announcement-schema";
+
+type getFilteredAnnouncementsParams = {
+	announcements: Announcement[];
+	searchQuery: string;
+};
+
+export function getFilteredAnnouncements({
+	announcements,
+	searchQuery
+}: getFilteredAnnouncementsParams) {
+	return announcements.filter((announcement) => {
+		return announcement.title.toLowerCase().includes(searchQuery.toLowerCase());
+	});
+}
