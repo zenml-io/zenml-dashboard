cfd2c52287049b717321b0a38073d4a189b98096
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2025-12-15 14:56:55 +0100

    feat: add cache expiry

---
+++ src/components/steps/step-sheet/use-expire-cache.ts
@@ -0,0 +1,36 @@
+import { useUpdateStep } from "@/data/steps/update-step";
+import { useQueryClient } from "@tanstack/react-query";
+import { useToast } from "@zenml-io/react-component-library";
+
+export function useExpireCache(stepId: string) {
+	const queryClient = useQueryClient();
+
+	const { toast } = useToast();
+
+	const { mutate: updateStep, isPending } = useUpdateStep({
+		onError: (e) => {
+			toast({
+				status: "error",
+				emphasis: "subtle",
+				description: e.message,
+				rounded: true
+			});
+		},
+		onSuccess() {
+			queryClient.invalidateQueries({
+				queryKey: ["steps"]
+			});
+		}
+	});
+
+	function handleExpireNow() {
+		updateStep({
+			id: stepId,
+			body: {
+				cache_expires_at: new Date().toISOString()
+			}
+		});
+	}
+
+	return { handleExpireNow, isPending };
+}
